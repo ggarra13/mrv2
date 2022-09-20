a1fa488025299dac5afe9bc7c4cc3d1f29118057
Gonzalo Garramuño <ggarra13@gmail.com>
2022-09-20 19:52:04 -0300

    Added runmet.sh to quickly compile tlRender and mrv2.

---
+++ runmet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export FLAGS=$@
+export TYPE=Debug
+
+if [[ $FLAGS == "" ]]; then
+    export FLAGS="-j 4"
+fi
+
+
+cd build/tlRender/etc/SuperBuild/tlRender/src/tlRender-build/
+
+make $FLAGS install
+
+cd -
+
+rm build/install/bin/mrViewer
+
+cd build/mrViewer/src/mrViewer2-build
+
+
+make $FLAGS install
+
+cd ../../..
