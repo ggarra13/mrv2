f3e09c0294da95036880ab42a00e42dbb0d07fe8
Gonzalo Garramuño <ggarra13@gmail.com>
2022-09-22 23:09:39 -0300

    Added runmea.sh script to build all without deleting build dir.

---
+++ runmea.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export FLAGS=$@
+export TYPE=Debug
+
+if [[ $FLAGS == "" ]]; then
+    export FLAGS="-j 4"
+fi
+
+
+mkdir -p build
+cd build
+
+
+cmake .. -D CMAKE_BUILD_TYPE=$TYPE -D CMAKE_INSTALL_PREFIX=$PWD/install -D CMAKE_PREFIX_PATH=$PWD/install  -D TLRENDER_BUILD_PROGRAMS=OFF -D TLRENDER_BUILD_EXAMPLES=FALSE -D TLRENDER_BUILD_TESTS=FALSE -D TLRENDER_BUILD_QT6=OFF -D TLRENDER_BUILD_QT5=FALSE
+
+cmake --build . $FLAGS --config $TYPE
+
+cd ..
