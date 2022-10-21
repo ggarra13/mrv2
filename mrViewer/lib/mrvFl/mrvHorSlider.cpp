bebebcce9eb1ee40538b9ac161711914d121029b
Gonzalo Garramuño <ggarra13@gmail.com>
2022-10-21 20:25:09 -0300

    Split mrvHorSlider into .h and .cpp file.

---
+++ mrViewer/lib/mrvFl/mrvHorSlider.cpp
@@ -0,0 +1,75 @@
+
+#include <cmath>
+
+#include "mrvHorSlider.h"
+
+#include "mrvFunctional.h"
+
+namespace mrv
+{
+
+  HorSlider::HorSlider( int X, int Y, int W, int H, const char* L ) :
+    Fl_Group( X, Y, W, H )
+  {
+    begin();
+                
+    auto uiValueW = new Widget<Fl_Float_Input>( X+90, Y, 30, H, L );
+    uiValue = uiValueW;
+    uiValue->labelsize( 12 );
+    auto uiSliderW = new Widget<Fl_Hor_Slider>( X+120, Y, W-130, H );
+    uiSlider = uiSliderW;
+    uiSlider->when(FL_WHEN_CHANGED);
+    auto uiResetW  = new Widget<Fl_Button>( X+W-10, Y, 10, H, "@-31+" );
+    uiReset = uiResetW;
+    uiReset->box( FL_NO_BOX );
+    end();
+    resizable( uiSlider );
+
+    uiSliderW->callback([=](auto s) {
+      double v = s->value();
+      char buf[32];
+      snprintf( buf, 32, "%.2g", v );
+      uiValue->value( buf );
+      do_callback();
+    } );
+                
+    uiValueW->callback( [=]( auto o ) {
+      double v = atof( o->value() );
+      uiSlider->value( v );
+      do_callback();
+    } );
+                
+    uiResetW->callback( [=]( auto o ) {
+      uiSlider->value( default_value );
+      uiSlider->do_callback();
+    } );
+
+    setRange( 0.F, 10.F );
+    setStep( 0.1F );
+  }
+
+  void HorSlider::setDefaultValue( double d ) noexcept
+  {
+    default_value = d;
+    uiSlider->value(d);
+    uiSlider->do_callback();
+  }
+
+  void HorSlider::setRange( double mn, double mx ) noexcept
+  {
+    uiSlider->bounds( mn, mx );
+  }
+
+  void HorSlider::setStep( double s ) noexcept
+  {
+    uiSlider->step( s );
+  }
+
+  void HorSlider::value( double x ) noexcept {
+    uiSlider->value( x );
+    uiSlider->do_callback();
+  }
+
+  double HorSlider::value() const noexcept { return uiSlider->value(); }
+
+}
--- mrViewer/lib/mrvFl/mrvHorSlider.h
+++ mrViewer/lib/mrvFl/mrvHorSlider.h
@@ -5,11 +5,8 @@
 #include <FL/Fl_Float_Input.H>
 #include <FL/Fl_Group.H>
 
-#include "mrvFunctional.h"
-
 namespace mrv
 {
-
 
     class HorSlider : public Fl_Group
     {
@@ -19,69 +16,14 @@ namespace mrv
         Fl_Button*      uiReset;
         double           default_value;
     public:
-        HorSlider( int X, int Y, int W, int H, const char* L = 0 ) :
-            Fl_Group( X, Y, W, H )
-            {
-                begin();
-                
-                auto uiValueW = new Widget<Fl_Float_Input>( X+90, Y, 30, H, L );
-                uiValue = uiValueW;
-                uiValue->labelsize( 12 );
-                auto uiSliderW = new Widget<Fl_Hor_Slider>( X+120, Y, W-130, H );
-                uiSlider = uiSliderW;
-                uiSlider->when(FL_WHEN_CHANGED);
-                auto uiResetW  = new Widget<Fl_Button>( X+W-10, Y, 10, H, "@-31+" );
-                uiReset = uiResetW;
-                uiReset->box( FL_NO_BOX );
-                end();
-                resizable( uiSlider );
+      HorSlider( int X, int Y, int W, int H, const char* L = 0 );
 
-                uiSliderW->callback([=](auto s) {
-                    double v = s->value();
-                    char buf[32];
-                    snprintf( buf, 32, "%.2g", v );
-                    uiValue->value( buf );
-                    do_callback();
-                } );
-                
-                uiValueW->callback( [=]( auto o ) {
-                    double v = atof( o->value() );
-                    uiSlider->value( v );
-                    do_callback();
-                } );
-                
-                uiResetW->callback( [=]( auto o ) {
-                    uiSlider->value( default_value );
-                    uiSlider->do_callback();
-                } );
+      void setDefaultValue( double d ) noexcept;
+      void setRange( double mn, double mx ) noexcept;
+      void setStep( double s ) noexcept;
+      void value( double x ) noexcept;
 
-                setRange( 0.F, 10.F );
-                setStep( 0.1F );
-            }
-
-        void setDefaultValue( double d )
-            {
-                default_value = d;
-                uiSlider->value(d);
-                uiSlider->do_callback();
-            }
-
-        void setRange( double mn, double mx )
-            {
-                uiSlider->bounds( mn, mx );
-            }
-
-        void setStep( double s )
-            {
-                uiSlider->step( s );
-            }
-
-        void value( double x ) {
-            uiSlider->value( x );
-            uiSlider->do_callback();
-        }
-
-        double value() const { return uiSlider->value(); }
+      double value() const noexcept;
     };
 
 
