bb9d24cc16aa32051df38b8e37993b211ad0e9a8
Gonzalo Garramuño <ggarra13@gmail.com>
2022-11-05 00:34:35 -0300

    Added copy_ffmpeg.sh to copy windows ffmpeg files.

---
+++ copy_ffmpeg.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cp -r /E/mabs/local64/bin-video/ffmpegSHARED/include/lib* /E/code/applications/mrv2/build_win64/install/include
+cp -r /E/mabs/local64/bin-video/ffmpegSHARED/bin/*.lib /E/code/applications/mrv2/build_win64/install/bin
+cp -r /E/mabs/local64/bin-video/ffmpegSHARED/bin/*.lib /E/code/applications/mrv2/build_win64/install/lib
+cp -r /E/mabs/local64/bin-video/ffmpegSHARED/bin/*.dll /E/code/applications/mrv2/build_win64/install/bin
+cp -r /E/code/lib/win64/bin/glew32.dll /E/code/applications/mrv2/build_win64/install/bin
