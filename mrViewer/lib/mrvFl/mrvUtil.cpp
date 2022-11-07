5d58780ff5bc680e4588d321ae87fedc38342b35
Gonzalo Garramuño <ggarra13@gmail.com>
2022-11-07 00:11:05 -0300

    Fixed char* instead of const char*.

--- mrViewer/lib/mrvFl/mrvUtil.cpp
+++ mrViewer/lib/mrvFl/mrvUtil.cpp
@@ -13,28 +13,28 @@ namespace
 
 namespace mrv
 {
-  Fl_SVG_Image* load_svg( const std::string& svg_name )
-  {
-    char* root =  fl_getenv( "MRV_ROOT" );
-    if ( !root ) root = "..";
+    Fl_SVG_Image* load_svg( const std::string& svg_name )
+    {
+        const char* root =  fl_getenv( "MRV_ROOT" );
+        if ( !root ) root = "..";
     
-    std::string svg_root = root;
-    svg_root += "/icons/";
+        std::string svg_root = root;
+        svg_root += "/icons/";
     
-    std::string file = svg_root + svg_name;
-    Fl_SVG_Image* svg = new Fl_SVG_Image( file.c_str() );
-    if ( !svg ) return nullptr;
+        std::string file = svg_root + svg_name;
+        Fl_SVG_Image* svg = new Fl_SVG_Image( file.c_str() );
+        if ( !svg ) return nullptr;
       
-    switch (svg->fail()) {
-    case Fl_Image::ERR_FILE_ACCESS:
-      // File couldn't load? show path + os error to user
-      LOG_ERROR( file << ": " << strerror(errno) );
-      return nullptr;
-    case Fl_Image::ERR_FORMAT:
-      // Parsing error
-      LOG_ERROR( file << ": couldn't decode image" );
-      return nullptr;
+        switch (svg->fail()) {
+        case Fl_Image::ERR_FILE_ACCESS:
+            // File couldn't load? show path + os error to user
+            LOG_ERROR( file << ": " << strerror(errno) );
+            return nullptr;
+        case Fl_Image::ERR_FORMAT:
+            // Parsing error
+            LOG_ERROR( file << ": couldn't decode image" );
+            return nullptr;
+        }
+        return svg;
     }
-    return svg;
-  }
 }
