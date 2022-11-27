a7f69f6b7ed108e7f11bbd3ddd6ff46831a385b8
Gonzalo Garramuño <ggarra13@gmail.com>
2022-11-27 11:38:52 -0300

    Added comment with instructions that allow calling it with the SDK.

--- windows/bat/vcvars_win64.bat
+++ windows/bat/vcvars_win64.bat
@@ -1,6 +1,10 @@
 
 REM Set the Visual Studio Environment.  Change this to the location of your
 REM Visual Studio.
+REM
+REM You can also call this command with an additional parameter to the SDK
+REM to use, like 8.1.
+REM
 
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
