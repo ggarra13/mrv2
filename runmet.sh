190563dc40c6fb2ea5a47f839771c2ff6f12a9f2
Gonzalo Garramuño <ggarra13@gmail.com>
2022-12-01 08:36:25 -0300

    Reverted change on how to compile mrv2 in this script.

--- runmet.sh
+++ runmet.sh
@@ -29,4 +29,25 @@ cmake --build . $FLAGS --config $CMAKE_BUILD_TYPE -t install
 
 cd -
 
-. $PWD/runmeq.sh
+#
+# This is needed on macOS which spits out linking errors otherwise
+#
+rm -f $BUILD_DIR/install/bin/mrViewer
+
+dir=$BUILD_DIR/mrViewer/src/mrViewer2-build
+
+if [[ ! -d $dir ]]; then
+    echo "mrViewer directory does not exist."
+    echo "Please run:"
+    echo " $ runme.sh [sameflags]"
+    exit 1
+fi
+
+cd $dir
+
+cmake --build . $FLAGS --config $CMAKE_BUILD_TYPE -v -t install
+
+cd -
+
+. $PWD/aux/build_end.sh
+
