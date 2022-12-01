708d16a48e7c3cf0f78010850e3eb1bb159b7264
Gonzalo Garramuño <ggarra13@gmail.com>
2022-12-01 06:12:47 -0300

    More additional instructions in the vcvars_win*.bat files.

--- windows/bat/vcvars_win32.bat
+++ windows/bat/vcvars_win32.bat
@@ -1,11 +1,14 @@
 
 REM Set the Visual Studio Environment.  Change this to the location of your
-REM Visual Studio.
+REM Visual Studio.  This file is for compiling in Windows 32 bits.
 REM
 REM You can also call this command with an additional parameter to the SDK
-REM to use, like 8.1.
+REM to use, like 8.1.. or nothing to pick the default SDK.
+REM
+REM 8.1 allows you to create an executable which is compatible with Winddows
+REM 8.1, 10, and 11.
 REM
 
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86 8.1
 
 C:\msys64\msys2_shell.cmd -use-full-path
