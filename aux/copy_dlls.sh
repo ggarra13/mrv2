94524f617b2e3be7795bb2a67abee6bf4feedaeb
Gonzalo Garramuño <ggarra13@gmail.com>
2022-12-03 11:01:40 -0300

    Removed exit 1 from copy_dlls.sh script.  Also removed copying of *.lib files from the bin directory.

--- aux/copy_dlls.sh
+++ aux/copy_dlls.sh
@@ -6,9 +6,6 @@ echo "copy_dlls.sh: Copying FFFMPEG from $FFMPEG_DIR to $BUILD_DIR"
 echo "Copying header files $BUILD_DIR/install/include"
 cp -r $FFMPEG_DIR/include/lib* $BUILD_DIR/install/include
 
-echo "Copying $FFMPEG_DIR/bin/*.lib to $BUILD_DIR/install/lib"
-cp -f $FFMPEG_DIR/bin/*.lib $BUILD_DIR/install/lib
-
 echo "Copying $FFMPEG_DIR/lib/*.lib to $BUILD_DIR/install/lib"
 cp -f $FFMPEG_DIR/lib/*.lib $BUILD_DIR/install/lib
 
@@ -20,4 +17,3 @@ cp -f ${LIBINTL_ROOT}/bin/libintl-8.dll $PWD/$BUILD_DIR/install/bin/
 echo "copy_dlls.sh: Copying ${LIBINTL_ROOT}/bin/libiconv-2.dll $PWD/$BUILD_DIR/install/bin/"
 cp -f ${LIBINTL_ROOT}/bin/libiconv-2.dll $PWD/$BUILD_DIR/install/bin/
 
-exit 1
