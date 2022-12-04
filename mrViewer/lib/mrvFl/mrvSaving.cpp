89260b6d42f8b8d57a1801172f7545b04f8d7f23
Gonzalo Garramuño <ggarra13@gmail.com>
2022-12-03 22:57:45 -0300

    Removed silly auxiliary function.

--- mrViewer/lib/mrvFl/mrvSaving.cpp
+++ mrViewer/lib/mrvFl/mrvSaving.cpp
@@ -1,4 +1,3 @@
-
 #include <string>
 #include <algorithm>
 #include <chrono>
@@ -32,13 +31,13 @@ namespace
 namespace mrv
 {
 
-    void _save_movie( const std::string& file, ViewerUI* ui )
+    void save_movie( const std::string& file, ViewerUI* ui )
     {
         Viewport* view = ui->uiView;
-        
+
         auto player = view->getTimelinePlayer();
         if (! player ) return;  // should never happen
-        
+
         // Time range.
         auto timeRange = player->inOutRange();
         auto startTime = timeRange.start_time();
@@ -46,8 +45,8 @@ namespace mrv
         auto currentTime = startTime;
 
         auto context  = ui->app->getContext();
-        auto timeline = player->timeline(); 
-        
+        auto timeline = player->timeline();
+
         // Render information.
         const auto& info = player->ioInfo();
         if (info.video.empty())
@@ -55,7 +54,7 @@ namespace mrv
             throw std::runtime_error("No video information");
         }
         auto renderSize = info.video[0].size;
-        
+
         // Create the renderer.
         auto render = gl::Render::create(context);
         gl::OffscreenBufferOptions offscreenBufferOptions;
@@ -66,7 +65,7 @@ namespace mrv
         // Create the writer.
         auto writerPlugin = ui->app->getContext()->getSystem<io::System>()->getPlugin(file::Path(file) );
 
-        if (!writerPlugin )    
+        if (!writerPlugin )
         {
             throw std::runtime_error(string::Format("{0}: Cannot open").arg(file));
         }
@@ -74,19 +73,19 @@ namespace mrv
         imaging::Info outputInfo;
         outputInfo.size = renderSize;
         outputInfo.pixelType = info.video[0].pixelType;
-        
+
         outputInfo = writerPlugin->getWriteInfo(outputInfo);
         if (imaging::PixelType::None == outputInfo.pixelType)
         {
             outputInfo.pixelType = imaging::PixelType::RGB_U8;
         }
-        
+
         auto outputImage = imaging::Image::create(outputInfo);
-        
+
         io::Info ioInfo;
         ioInfo.video.push_back(outputInfo);
         ioInfo.videoTime = timeRange;
-        
+
         auto writer = writerPlugin->write(file::Path(file), ioInfo);
         if (!writer)
         {
@@ -96,7 +95,7 @@ namespace mrv
 
         int64_t startFrame = startTime.to_frames();
         int64_t endFrame   = endTime.to_frames();
-        
+
         ProgressReport progress( ui->uiMain, startFrame, endFrame );
         progress.show();
 
@@ -110,7 +109,7 @@ namespace mrv
                         otime::RationalTime(1.0, currentTime.rate())) });
 
             DBGM0( "buffer= " << buffer << " ID= " << buffer->getID() );
-            
+
             // If progress window is closed, exit loop.
 
             // Get the videoData
@@ -118,7 +117,7 @@ namespace mrv
 
             // This does not work!
             //view->videoCallback( videoData, player );
-            
+
             if (! progress.tick() ) break;
 
             // Render the video.
@@ -129,7 +128,7 @@ namespace mrv
                               { math::BBox2i(0, 0,
                                              renderSize.w, renderSize.h ) });
             render->end();
-            
+
             glPixelStorei(GL_PACK_ALIGNMENT, outputInfo.layout.alignment);
             glPixelStorei(GL_PACK_SWAP_BYTES, outputInfo.layout.endian != memory::getEndian());
             const GLenum format = gl::getReadPixelsFormat(outputInfo.pixelType);
@@ -146,23 +145,19 @@ namespace mrv
                 format,
                 type,
                 outputImage->getData());
-            
+
             writer->writeVideo(currentTime, outputImage);
-            
+
             currentTime += otime::RationalTime(1, currentTime.rate());
             if (currentTime > endTime)
             {
                 running = false;
             }
-            
+
         }
 
         timeline->setActiveRanges({ otime::TimeRange( startTime, endTime ) });
         player->seek( currentTime );
     }
 
-    void save_movie( const std::string& file, ViewerUI* ui )
-    {
-        _save_movie( file, ui );
-    }
 }
