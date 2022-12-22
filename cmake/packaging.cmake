91ea97f7d113236cf27293f6f161b88ead87bea7
Gonzalo Garramuño <ggarra13@gmail.com>
2022-12-22 08:59:47 -0300

    Made desktop file be created inside PROJECT_BINARY_DIR.

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -50,9 +50,9 @@ elseif(UNIX)
 
 
   configure_file( ${ROOT_DIR}/etc/Linux/mrViewer2.desktop.in
-    "${ROOT_DIR}/etc/mrViewer2-v${SHORTVERSION}.desktop" )
+    "${PROJECT_BINARY_DIR}/mrViewer2-v${SHORTVERSION}.desktop" )
 
-  install(FILES "${ROOT_DIR}/etc/mrViewer2-v${SHORTVERSION}.desktop"
+  install(FILES "${PROJECT_BINARY_DIR}/mrViewer2-v${SHORTVERSION}.desktop"
     DESTINATION share/applications COMPONENT applications)
   install(FILES ${ROOT_DIR}/etc/mrViewer2.png
     DESTINATION share/icons/hicolor/32x32/apps COMPONENT applications)
