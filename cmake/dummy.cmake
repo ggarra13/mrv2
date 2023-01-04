8d1e13839067dc4fbd73d52cf85f325abb5200c3
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-04 09:18:35 -0300

    Added script to clean up .a and include files from macOS bundle.

---
+++ cmake/dummy.cmake
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2 (mrViewer2)
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+
+#
+# This is a dummy script file used as CPACK_INSTALL_SCRIPT so the
+# install variables are set.
+# 
--- cmake/prepackage.cmake
+++ cmake/prepackage.cmake
@@ -2,22 +2,11 @@
 # mrv2 (mrViewer2)
 # Copyright Contributors to the mrv2 Project. All rights reserved.
 
-include( ../../../../../cmake/version.cmake )
-set( CPACK_PACKAGE_VERSION_MAJOR "${mrViewer2_VERSION_MAJOR}" )
-set( CPACK_PACKAGE_VERSION_MINOR "${mrViewer2_VERSION_MINOR}" )
-set( CPACK_PACKAGE_VERSION_PATCH "${mrViewer2_VERSION_PATCH}" )
-set( CPACK_PACKAGE_VERSION "${mrViewer2_VERSION_MAJOR}.${mrViewer2_VERSION_MINOR}.${mrViewer2_VERSION_PATCH}")
-set( SHORTVERSION ${CPACK_PACKAGE_VERSION} )
-set( mrv2_dir "mrViewer2-${SHORTVERSION}-Darwin" )
-set( cpack_dir ${CMAKE_CURRENT_BINARY_DIR}/_CPack_Packages/Darwin/Bundle/${mrv2_dir}/mrViewer2.app/Contents/Resources/ )
-
-
-
 message( STATUS "CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}" )
 message( STATUS "CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}" )
 message( STATUS "CMAKE_CURRENT_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}" )
-message( STATUS "cpack_dir=${cpack_dir}" )
+message( STATUS $ENV{DESTDIR} )
 
-file( GLOB STATIC_LIBS "${cpack_dir}/lib/*.a" )
-file( REMOVE "${STATIC_LIBS}" )
-file( REMOVE_RECURSE "${cpack_dir}/include" )
+file( GLOB STATIC_LIBS "${CMAKE_INSTALL_PREFIX}/lib/*.a" )
+file( REMOVE ${STATIC_LIBS} )
+file( REMOVE_RECURSE "${CMAKE_INSTALL_PREFIX}/include" )
