861727dbd30949c7bfe56da1a562eb9e269cee6e
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-04 09:27:29 -0300

    Added inline docs.

--- cmake/prepackage.cmake
+++ cmake/prepackage.cmake
@@ -2,11 +2,14 @@
 # mrv2 (mrViewer2)
 # Copyright Contributors to the mrv2 Project. All rights reserved.
 
-message( STATUS "CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}" )
-message( STATUS "CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}" )
-message( STATUS "CMAKE_CURRENT_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}" )
-message( STATUS $ENV{DESTDIR} )
+#
+# Remove .a files from packaging lib/ directory
+#
 
 file( GLOB STATIC_LIBS "${CMAKE_INSTALL_PREFIX}/lib/*.a" )
 file( REMOVE ${STATIC_LIBS} )
+
+#
+# Remove include files from packaging directory
+#
 file( REMOVE_RECURSE "${CMAKE_INSTALL_PREFIX}/include" )
