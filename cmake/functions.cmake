70e77dfecb8f7d887b8dd473d02e533bea9ddd0c
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-10 14:55:52 -0300

    Made all CMake files 4 spaces tabulation.

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -3,18 +3,18 @@
 # Copyright Contributors to the mrv2 Project. All rights reserved.
 
 function (FLTK_RUN_FLUID TARGET SOURCES)
-  set (CXX_FILES)
-  foreach (src ${SOURCES})
-    if ("${src}" MATCHES "\\.fl$")
-      string(REGEX REPLACE "(.*/)?(.*).fl" \\2 basename ${src})
-      add_custom_command(
-	OUTPUT "${basename}.cxx" "${basename}.h"
-	COMMAND ${FLTK_FLUID_EXECUTABLE} -c ${CMAKE_CURRENT_SOURCE_DIR}/${src}
-	DEPENDS ${src}
-	MAIN_DEPENDENCY ${src}
-      )
-      list (APPEND CXX_FILES "${CMAKE_CURRENT_BINARY_DIR}/${basename}.cxx")
-    endif ("${src}" MATCHES "\\.fl$")
-  endforeach ()
-  set (${TARGET} ${CXX_FILES} PARENT_SCOPE)
+    set (CXX_FILES)
+    foreach (src ${SOURCES})
+	if ("${src}" MATCHES "\\.fl$")
+	    string(REGEX REPLACE "(.*/)?(.*).fl" \\2 basename ${src})
+	    add_custom_command(
+		OUTPUT "${basename}.cxx" "${basename}.h"
+		COMMAND ${FLTK_FLUID_EXECUTABLE} -c ${CMAKE_CURRENT_SOURCE_DIR}/${src}
+		DEPENDS ${src}
+		MAIN_DEPENDENCY ${src}
+		)
+	    list (APPEND CXX_FILES "${CMAKE_CURRENT_BINARY_DIR}/${basename}.cxx")
+	endif ("${src}" MATCHES "\\.fl$")
+    endforeach ()
+    set (${TARGET} ${CXX_FILES} PARENT_SCOPE)
 endfunction (FLTK_RUN_FLUID TARGET SOURCES)
--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -11,9 +11,9 @@ set( SHORTVERSION ${CPACK_PACKAGE_VERSION} )
 set( CPACK_PACKAGE_CONTACT "ggarra13@gmail.com")
 
 if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-  set( MRV_OS_BITS 64 )
+    set( MRV_OS_BITS 64 )
 elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
-  set( MRV_OS_BITS 32 )
+    set( MRV_OS_BITS 32 )
 endif()
 
 set( mrViewerShortName "mrViewer2-v${SHORTVERSION}-${CMAKE_SYSTEM_NAME}-${MRV_OS_BITS}" )
@@ -27,101 +27,101 @@ set( CPACK_PACKAGE_INSTALL_DIRECTORY ${mrViewerShortName} )
 set( ROOT_DIR ${CMAKE_SOURCE_DIR} )
 
 if( APPLE )
-  set(CPACK_GENERATOR Bundle )
+    set(CPACK_GENERATOR Bundle )
 
-  set( CPACK_INSTALL_SCRIPTS ${CMAKE_SOURCE_DIR}/../cmake/dummy.cmake )
-  set( CPACK_PRE_BUILD_SCRIPTS ${CMAKE_SOURCE_DIR}/../cmake/prepackage.cmake )
-  
-  set( INSTALL_NAME ${PROJECT_NAME} )
+    set( CPACK_INSTALL_SCRIPTS ${CMAKE_SOURCE_DIR}/../cmake/dummy.cmake )
+    set( CPACK_PRE_BUILD_SCRIPTS ${CMAKE_SOURCE_DIR}/../cmake/prepackage.cmake )
 
-  configure_file(
-  ${ROOT_DIR}/etc/macOS/startup.sh.in
-  ${PROJECT_BINARY_DIR}/startup.sh)
+    set( INSTALL_NAME ${PROJECT_NAME} )
 
-  configure_file(
-  ${ROOT_DIR}/etc/macOS/Info.plist.in
-  ${PROJECT_BINARY_DIR}/Info.plist )
+    configure_file(
+	${ROOT_DIR}/etc/macOS/startup.sh.in
+	${PROJECT_BINARY_DIR}/startup.sh)
 
-  set(CPACK_PACKAGE_ICON ${ROOT_DIR}/etc/macOS/mrViewer2.icns )
-  set(CPACK_BUNDLE_NAME ${INSTALL_NAME} )
-  set(CPACK_BUNDLE_ICON ${ROOT_DIR}/etc/macOS/mrViewer2.icns )
-  set(CPACK_BUNDLE_PLIST ${PROJECT_BINARY_DIR}/Info.plist )
-  set(CPACK_BUNDLE_STARTUP_COMMAND ${PROJECT_BINARY_DIR}/startup.sh)
+    configure_file(
+	${ROOT_DIR}/etc/macOS/Info.plist.in
+	${PROJECT_BINARY_DIR}/Info.plist )
+
+    set(CPACK_PACKAGE_ICON ${ROOT_DIR}/etc/macOS/mrViewer2.icns )
+    set(CPACK_BUNDLE_NAME ${INSTALL_NAME} )
+    set(CPACK_BUNDLE_ICON ${ROOT_DIR}/etc/macOS/mrViewer2.icns )
+    set(CPACK_BUNDLE_PLIST ${PROJECT_BINARY_DIR}/Info.plist )
+    set(CPACK_BUNDLE_STARTUP_COMMAND ${PROJECT_BINARY_DIR}/startup.sh)
 elseif(UNIX)
 
-  set( CPACK_PACKAGING_INSTALL_PREFIX "/usr/local" )
+    set( CPACK_PACKAGING_INSTALL_PREFIX "/usr/local" )
 
-  configure_file( ${ROOT_DIR}/etc/Linux/mrViewer2.desktop.in
-    "${PROJECT_BINARY_DIR}/mrViewer2-v${SHORTVERSION}.desktop" )
+    configure_file( ${ROOT_DIR}/etc/Linux/mrViewer2.desktop.in
+	"${PROJECT_BINARY_DIR}/mrViewer2-v${SHORTVERSION}.desktop" )
 
-  install(FILES "${PROJECT_BINARY_DIR}/mrViewer2-v${SHORTVERSION}.desktop"
-    DESTINATION share/applications COMPONENT applications)
-  install(FILES ${ROOT_DIR}/etc/mrViewer2.png
-    DESTINATION share/icons/hicolor/32x32/apps COMPONENT applications)
+    install(FILES "${PROJECT_BINARY_DIR}/mrViewer2-v${SHORTVERSION}.desktop"
+	DESTINATION share/applications COMPONENT applications)
+    install(FILES ${ROOT_DIR}/etc/mrViewer2.png
+	DESTINATION share/icons/hicolor/32x32/apps COMPONENT applications)
 
-  set(CPACK_GENERATOR DEB RPM TGZ )
+    set(CPACK_GENERATOR DEB RPM TGZ )
 
-  configure_file(
-    ${ROOT_DIR}/etc/Linux/postinst.in
-    ${PROJECT_BINARY_DIR}/etc/Linux/postinst
-    @ONLY)
-  configure_file(
-    ${ROOT_DIR}/etc/Linux/postrm.in
-    ${PROJECT_BINARY_DIR}/etc/Linux/postrm
-  @ONLY)
+    configure_file(
+	${ROOT_DIR}/etc/Linux/postinst.in
+	${PROJECT_BINARY_DIR}/etc/Linux/postinst
+	@ONLY)
+    configure_file(
+	${ROOT_DIR}/etc/Linux/postrm.in
+	${PROJECT_BINARY_DIR}/etc/Linux/postrm
+	@ONLY)
 
-  # Set RPM options.
+    # Set RPM options.
 
-  set(CPACK_RPM_PACKAGE_NAME ${PROJECT_NAME})
-  set(CPACK_RPM_PACKAGE_RELOCATABLE true)
-  set(CPACK_RPM_PACKAGE_AUTOREQ false)
-  set(CPACK_RPM_PACKAGE_AUTOPROV true)
-  set(CPACK_RPM_COMPRESSION_TYPE gzip )
+    set(CPACK_RPM_PACKAGE_NAME ${PROJECT_NAME})
+    set(CPACK_RPM_PACKAGE_RELOCATABLE true)
+    set(CPACK_RPM_PACKAGE_AUTOREQ false)
+    set(CPACK_RPM_PACKAGE_AUTOPROV true)
+    set(CPACK_RPM_COMPRESSION_TYPE gzip )
 
-  set(
-  CPACK_RPM_POST_INSTALL_SCRIPT_FILE
-  ${PROJECT_BINARY_DIR}/etc/Linux/postinst)
-  set(
-  CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE
-  ${PROJECT_BINARY_DIR}/etc/Linux/postrm)
+    set(
+	CPACK_RPM_POST_INSTALL_SCRIPT_FILE
+	${PROJECT_BINARY_DIR}/etc/Linux/postinst)
+    set(
+	CPACK_RPM_POST_UNINSTALL_SCRIPT_FILE
+	${PROJECT_BINARY_DIR}/etc/Linux/postrm)
 
 
-  # set Debian options.
+    # set Debian options.
 
-  set(CPACK_DEBIAN_PACKAGE_NAME ${PROJECT_NAME})
+    set(CPACK_DEBIAN_PACKAGE_NAME ${PROJECT_NAME})
 
-  set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
-  ${PROJECT_BINARY_DIR}/etc/Linux/postinst
-  ${PROJECT_BINARY_DIR}/etc/Linux/postrm)
+    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA
+	${PROJECT_BINARY_DIR}/etc/Linux/postinst
+	${PROJECT_BINARY_DIR}/etc/Linux/postrm)
 
-  set(CPACK_SET_DESTDIR true) # Needed
-  set(CPACK_INSTALL_PREFIX /usr/local/${mrViewerShortName})
+    set(CPACK_SET_DESTDIR true) # Needed
+    set(CPACK_INSTALL_PREFIX /usr/local/${mrViewerShortName})
 
 else()
 
-  # There is a bug in NSIS that does not handle full unix paths properly. Make
-  # sure there is at least one set of four (4) backlasshes.
-  set(CPACK_NSIS_MODIFY_PATH ON)
+    # There is a bug in NSIS that does not handle full unix paths properly. Make
+    # sure there is at least one set of four (4) backlasshes.
+    set(CPACK_NSIS_MODIFY_PATH ON)
 
-  set(CPACK_GENERATOR ZIP NSIS)
+    set(CPACK_GENERATOR ZIP NSIS)
 
-  set(CPACK_NSIS_MUI_ICON ${ROOT_DIR}/icons/viewer.ico)
-  set(CPACK_NSIS_MUI_UNICON ${ROOT_DIR}/icons/viewer.ico)
+    set(CPACK_NSIS_MUI_ICON ${ROOT_DIR}/icons/viewer.ico)
+    set(CPACK_NSIS_MUI_UNICON ${ROOT_DIR}/icons/viewer.ico)
 
-  if (MRV_OS_BITS EQUAL 32)
-    set( CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES" )
-  else()
-    set( CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64" )
-  endif()
+    if (MRV_OS_BITS EQUAL 32)
+	set( CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES" )
+    else()
+	set( CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64" )
+    endif()
 
-  set( CPACK_NSIS_DISPLAY_NAME "mrViewer2-${MRV_OS_BITS} v${SHORTVERSION}" )
-  set( CPACK_NSIS_PACKAGE_NAME "mrViewer2" )
-  set( CPACK_PACKAGE_VENDOR "FilmAura" )
-  set( CPACK_PACKAGE_INSTALL_DIRECTORY ${mrViewerPackageName})
-  set( CPACK_PACKAGE_EXECUTABLES "mrViewer" "mrViewer2-${MRV_OS_BITS} v${SHORTVERSION}" )
-  set( CPACK_CREATE_DESKTOP_LINKS "mrViewer" "mrViewer2-${MRV_OS_BITS} v${SHORTVERSION}" )
+    set( CPACK_NSIS_DISPLAY_NAME "mrViewer2-${MRV_OS_BITS} v${SHORTVERSION}" )
+    set( CPACK_NSIS_PACKAGE_NAME "mrViewer2" )
+    set( CPACK_PACKAGE_VENDOR "FilmAura" )
+    set( CPACK_PACKAGE_INSTALL_DIRECTORY ${mrViewerPackageName})
+    set( CPACK_PACKAGE_EXECUTABLES "mrViewer" "mrViewer2-${MRV_OS_BITS} v${SHORTVERSION}" )
+    set( CPACK_CREATE_DESKTOP_LINKS "mrViewer" "mrViewer2-${MRV_OS_BITS} v${SHORTVERSION}" )
 
-  set( CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON )
+    set( CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON )
 
 endif()
 
