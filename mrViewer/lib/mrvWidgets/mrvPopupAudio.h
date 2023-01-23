39a8dcd6a45ba78ea14740235a385f4a72ac9482
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-23 03:36:38 -0300

    Simplified audio muting to a toggle when only one audio track is loaded.

---
+++ mrViewer/lib/mrvWidgets/mrvPopupAudio.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// mrv2 (mrViewer2)
+// Copyright Contributors to the mrv2 Project. All rights reserved.
+
+#pragma once
+
+#include "mrvWidgets/mrvPopupMenu.h"
+
+namespace mrv {
+
+class PopupAudio : public PopupMenu
+{
+public:
+    PopupAudio(int,int,int,int,const char* =0);
+    virtual ~PopupAudio() {};
+    virtual int handle(int e) override;
+};
+
+}
