4bf58a7aaa4db23ab17e9d511cc424dcf1085066
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-29 19:24:44 -0300

    Updated github_release.cmake with output.

--- cmake/github_release.cmake
+++ cmake/github_release.cmake
@@ -16,39 +16,35 @@ include( cmake/version.cmake )
 
 find_package( Git REQUIRED )
 
-#
-# Delete local tag if available
-#
-message( STATUS "Remove packages directory" )
-execute_process( COMMAND rm -rf packages )
 
 #
 # Delete local tag if available
 #
 message( STATUS "Remove local tag v${mrv2_VERSION}" )
-execute_process( COMMAND ${GIT_EXECUTABLE} tag -d v${mrv2_VERSION} )
+execute_process( COMMAND ${GIT_EXECUTABLE} tag -d v${mrv2_VERSION}
+		 OUTPUT_VARIABLE _output  )
+message( STATUS "${_output}" )
 
 #
 # Delete remote tag if available
 #
 message( STATUS "Remove remote tag v${mrv2_VERSION}" )
-execute_process( COMMAND ${GIT_EXECUTABLE} push --delete origin v${mrv2_VERSION} )
+execute_process( COMMAND ${GIT_EXECUTABLE} push --delete origin v${mrv2_VERSION}                 OUTPUT_VARIABLE _output )
+message( STATUS "${_output}" )
 
 
 #
 # Mark current repository with a new tag
 #
 message( STATUS "Create local tag v${mrv2_VERSION}" )
-execute_process( COMMAND ${GIT_EXECUTABLE} tag v${mrv2_VERSION} )
+execute_process( COMMAND ${GIT_EXECUTABLE} tag v${mrv2_VERSION}
+		 OUTPUT_VARIABLE _output )
+message( STATUS "${_output}" )
 
 #
 # Send new tag to repository
 #
 message( STATUS "Create remote tag v${mrv2_VERSION}" )
-execute_process( COMMAND ${GIT_EXECUTABLE} push origin v${mrv2_VERSION} )
-
-#
-# Start runme_docker.sh script
-#
-message( STATUS "Run docker build with tag v${mrv2_VERSION}" )
-execute_process( COMMAND runme_docker.sh )
+execute_process( COMMAND ${GIT_EXECUTABLE} push origin v${mrv2_VERSION}
+		 OUTPUT_VARIABLE _output )
+message( STATUS "${_output}" )
