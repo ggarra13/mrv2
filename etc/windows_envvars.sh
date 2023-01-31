252e30ee6a7f38e9c60ff1573afee1a7bf63a4a6
Gonzalo Garramuño <ggarra13@gmail.com>
2023-01-31 00:42:18 -0300

    Updated window directory to not use ${ARCH}.

--- etc/windows_envvars.sh
+++ etc/windows_envvars.sh
@@ -10,13 +10,17 @@ if [[ $KERNEL == *Msys* ]]; then
     fi
 fi
 
+bits=64
+if [[ "$ARCH" != "amd64" ]]; then
+    bits=32
+fi
 
 # Set the location of libintl on windows 32 and 64 bits.
 # On Linux and macOS this is picked from /usr/local
-export LIBINTL_ROOT=$PWD/windows/win${ARCH}/
+export LIBINTL_ROOT=$PWD/windows/win${bits}/
 
 #
 # Set the location of ffmpeg on Windows.
 # We copy the contents of this directory to the build directory.
 #
-export FFMPEG_DIR=$PWD/windows/win${ARCH}/
+export FFMPEG_DIR=$PWD/windows/win${bits}/
