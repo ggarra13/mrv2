51a2b1e4c2ebf44b360bc93d5f3ad667c33d115f
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-01 22:22:20 -0300

    Fixed copying of default build settings.

--- etc/copy_dlls.sh
+++ etc/copy_dlls.sh
@@ -17,7 +17,7 @@ if [[ -d ${MABS_ROOT} ]]; then
     echo "We located a media-autobuild_suite install"
     echo "******************************************"
 
-    if [[ ! -e $ffmpeg_options ]]; then
+    if [[ -d $mabs_build && ! -e $ffmpeg_options ]]; then
 	echo "cp -r $PWD/windows/media-autobuild_suite/build/* $mabs_build"
 	cp -r $PWD/windows/media-autobuild_suite/build/* $mabs_build
 	echo "You have not run the media-autobuild_suite yet."
