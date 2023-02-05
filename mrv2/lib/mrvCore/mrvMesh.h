c287c6d5717e3569e621e9a163c05776566123fe
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-04 23:48:23 -0300

    Added missing files.

---
+++ mrv2/lib/mrvCore/mrvMesh.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2021-2023 Darby Johnston
+// All rights reserved.
+
+#pragma once
+
+#include <stddef.h>
+#include <algorithm>
+
+#include <tlCore/Math.h>
+#include <tlCore/Mesh.h>
+
+namespace mrv
+{
+    using namespace tl;
+    
+    //! Create a an environment cube triangle mesh.
+    geom::TriangleMesh3 createEnvCube(float size = 1.0F);
+}
+
