4eb90dfcd61ab044b46a1eb8e561e481dc83da3e
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-06 11:48:00 -0300

    Refactored git commands into etc/prepare_releae.sh

--- github_release.sh
+++ github_release.sh
@@ -6,70 +6,25 @@
 #
 # Read some auxiliary functions
 #
+. ./etc/prepare_release.sh
 
-. ./etc/functions.sh
 
-extract_version
-
-#
-# SOME DEFINES
-#
-GIT_EXECUTABLE=git
 DOCKER_EXECUTABLE=docker
-
-
-#
-# Pull last changes
-#
-echo "Switch to main and pull changes"
-${GIT_EXECUTABLE} switch main && ${GIT_EXECUTABLE} pull
-
-echo "Now push any commited changes"
-${GIT_EXECUTABLE} push
-
-echo "--------------------------------"
-echo "  Will release v${mrv2_VERSION}"
-echo "--------------------------------"
-
-
-
-#
-# Delete local tag if available
-#
-echo "Remove local tag v${mrv2_VERSION}"
-${GIT_EXECUTABLE} tag -d v${mrv2_VERSION}
-
-#
-# Delete remote tag if available
-#
-echo "Remove remote tag v${mrv2_VERSION}"
-${GIT_EXECUTABLE} push --delete origin v${mrv2_VERSION}
-
-
-
-#
-# Mark current repository with a new tag
-#
-echo "Create local tag v${mrv2_VERSION}"
-${GIT_EXECUTABLE} tag v${mrv2_VERSION}
-
-#
-# Send new tag to repository
-#
-echo "Create remote tag v${mrv2_VERSION}"
-${GIT_EXECUTABLE} push origin v${mrv2_VERSION}
 
 #
 # Stop all running containers
 #
 echo "Stopping all containers..."
-${DOCKER_EXECUTABLE} stop $(${DOCKER_EXECUTABLE} ps -aq)
+containers=`${DOCKER_EXECUTABLE} ps -aq`
+if [[ $containers != "" ]]; then
+    ${DOCKER_EXECUTABLE} stop $containers
 
-#
-# Remove all containers
-#
-echo "Removing all containers..."
-${DOCKER_EXECUTABLE} rm $(${DOCKER_EXECUTABLE} ps -aq)
+    #
+    # Remove all containers
+    #
+    echo "Removing all containers..."
+    ${DOCKER_EXECUTABLE} rm $contatiners
+fi
 
 #
 # Remove the images if present
