6f6a3e357d98fdf84014cefaa9ce5e1476ff280f
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-09 16:15:47 -0300

    Checked for nvidia_drm driver more accurately.

--- mrv2/bin/environment.sh
+++ mrv2/bin/environment.sh
@@ -24,7 +24,7 @@ fi
 
 
 #
-# Platform specific directory for mrViewer
+# Platform specific directory for mrV2
 #
 dir=${dir%/*}
 
@@ -40,16 +40,19 @@ export DYLD_FALLBACK_LIBRARY_PATH="${LD_LIBRARY_PATH}"
 # as Wayland is still too buggy.
 #
 if [[ "$XDG_SESSION_TYPE" == "wayland" && "$FLTK_BACKEND" == "" ]]; then
-    gfx_card=`lspci | grep 'VGA.*NVIDIA'`
+    gfx_card=`lspci -k | grep -EA3 'VGA|3D|Display' | grep nvidia_drm`
     if [[ $gfx_card != "" ]]; then
-	echo "Wayland support with NVidia cards is currently buggy."
-	echo "If you still want to run FLTK applications with Wayland,"
-	echo "set the environment variable FLTK_BACKEND to wayland, like:"
+	echo " Wayland support with NVidia drivers is currently buggy."
+	echo " If you still want to run FLTK applications with Wayland,"
+	echo " set the environment variable FLTK_BACKEND to wayland, like:"
 	echo ""
 	echo "   export FLTK_BACKEND=wayland"
 	echo ""
 	if [[ "$FLTK_BACKEND" == "" ]]; then
-	    echo "Setting the environment variable FLTK_BACKEND=x11."
+	    echo " Setting the environment variable FLTK_BACKEND=x11."
+	    echo ""
+	    echo " However, we recommend you log in into an X11 server or"
+	    echo " use the Xorg.org noveau drivers instead."
 	    echo ""
 	    export FLTK_BACKEND=x11
 	fi
