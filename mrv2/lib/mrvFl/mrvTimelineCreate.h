822cfbc418a7507de7e668ce1a776f7f52f92ca6
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-20 22:08:14 -0300

    Removed GCC pragmas.

--- mrv2/lib/mrvFl/mrvTimelineCreate.h
+++ mrv2/lib/mrvFl/mrvTimelineCreate.h
@@ -15,31 +15,23 @@
 #include <opentimelineio/composable.h>
 #include <opentimelineio/item.h>
 #include <opentimelineio/timeline.h>
-#include <opentimelineio/externalReference.h>
-#include <opentimelineio/imageSequenceReference.h>
 
 
 
 #include "mrvApp/mrvFilesModel.h"
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-#pragma GCC diagnostic ignored "-Wc++17-compat"
 
 namespace tl
 {
     namespace timeline
     {
-		using namespace tl;
-		using namespace tl::timeline;
 
         otio::SerializableObject::Retainer<otio::Timeline> create(
             const std::vector<std::shared_ptr<mrv::FilesModelItem> >& fileItems,
             const std::shared_ptr<system::Context>& context,
             const Options& options = Options(),
             const std::shared_ptr<ReadCache>& readCache = nullptr);
-
-#pragma GCC diagnostic pop
-		
+   	
     }
+	
 } // namespace tl
