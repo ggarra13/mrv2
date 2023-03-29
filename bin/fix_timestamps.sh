414f628e089fd068779217511bb8f8b8de4eb81c
Gonzalo Garramuño <ggarra13@gmail.com>
2023-03-29 04:05:33 -0300

    Re-coded add_License.rb in python.
    Added licenses.

--- bin/fix_timestamps.sh
+++ bin/fix_timestamps.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2
+# Copyright Contributors to the mrv2 Project. All rights reserved.
 
 #
 # This fixes a problem with times when switching in the same file system from
