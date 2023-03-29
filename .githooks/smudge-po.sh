9c30f212ae6455389ab880cc53719a7230777d7e
Gonzalo Garramuño <ggarra13@gmail.com>
2023-03-28 22:05:36 -0300

    Fixed smudge-po.sh file?

--- .githooks/smudge-po.sh
+++ .githooks/smudge-po.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-filename="$PWD/$1"
-cat $filename
+rootdir=$(git rev-parse --show-toplevel)
+filename=$(git ls-files --full-name -- "${1}")
+fullpath="${rootdir}/${filename}"
+awk '/^msgid/ { printf "%s%s\n", $0, NR == 1 ? "": "\n" } /^msgstr/ { print }' "${fullpath}"
