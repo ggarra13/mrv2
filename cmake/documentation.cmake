26d99c431751f9b7bbb50f162969dd0336273c2d
Gonzalo Garramuño <ggarra13@gmail.com>
2023-04-04 21:47:09 -0300

    Fixed permissions issue in shell scripts  (mrv2.sh).

--- cmake/documentation.cmake
+++ cmake/documentation.cmake
@@ -41,7 +41,7 @@ foreach( LANGUAGE ${LANGUAGES} )
 
     # Make sure document.py is executable
     file(CHMOD ${DOCUMENT_OUT}
-        PERMISSIONS
+        FILE_PERMISSIONS
         OWNER_READ OWNER_WRITE OWNER_EXECUTE
         GROUP_READ GROUP_EXECUTE
         WORLD_READ WORLD_EXECUTE)
