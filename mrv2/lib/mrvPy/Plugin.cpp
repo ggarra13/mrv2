a8a05c4eb514b29fd2dd8dce208a23f61f0757f2
Gonzalo Garramuño <ggarra13@gmail.com>
2023-06-20 07:38:41 -0300

    Started work on python plug-in system.

---
+++ mrv2/lib/mrvPy/Plugin.cpp
@@ -0,0 +1,33 @@
+
+#include <map>
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+namespace py = pybind11;
+
+#include "mrvCore/mrvHome.h"
+#include "mrvCore/mrvI8N.h"
+
+namespace mrv
+{
+    struct Plugin
+    {
+        std::map<std::string, std::string> get_menu_entries() const
+        {
+            std::map<std::string, std::string> out;
+            return out;
+        }
+    };
+} // namespace mrv
+
+void mrv2_python_plugins(pybind11::module& m)
+{
+    using namespace mrv;
+
+    py::class_<Plugin>(m, "Plugin")
+        .def(py::init<>())
+        .def(
+            "get_menu_entries", &Plugin::get_menu_entries,
+            _("Add menu entries as a python dictionary."))
+        .doc() = _("Base class for python plug-ins.");
+}
