67ae926179f277980772855541d7a4e9d1615e74
Gonzalo Garramuño <ggarra13@gmail.com>
2023-06-21 06:38:53 -0300

    Fixed FFMPEG Windows GPL script.
    Updated to latest tlRender.

--- ffmpeg_windows_gpl.sh
+++ ffmpeg_windows_gpl.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #
-# This script compiles a GPL version of ffmpeg
+# This script compiles a GPL version of ffmpeg with libx264 support, but no
+# libvpx support
 #
 
 . etc/build_dir.sh
@@ -34,7 +35,6 @@ mkdir -p install
 # GETTING SOURCES
 # 
 cd sources
-git clone --depth 1 https://github.com/videolan/x265.git
 git clone --depth 1 https://code.videolan.org/videolan/x264.git
 git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git
 
@@ -53,49 +53,41 @@ sed -i 's/host_os = mingw/host_os = msys/' configure
 cd $ROOT_DIR/build
 
 
-mkdir -p x265
-cd x265
-CC=cl ./../../sources/x265/configure --prefix=./../../installed --enable-shared
+mkdir -p x264
+cd x264
+CC=cl ./../../sources/x264/configure --prefix=./../../installed --enable-shared
 make -j ${CPU_CORES}
 make install
-mv ./../../installed/lib/libx265.dll.lib ./../../installed/lib/libx265.lib
+mv ./../../installed/lib/libx264.dll.lib ./../../installed/lib/libx264.lib
 
+cd $ROOT_DIR/build
+mkdir -p ffmpeg
+cd ffmpeg
 
-# mkdir -p x264
-# cd x264
-# CC=cl ./../../sources/x264/configure --prefix=./../../installed --enable-shared
-# make -j ${CPU_CORES}
-# make install
-# mv ./../../installed/lib/libx264.dll.lib ./../../installed/lib/libx264.lib
+export CC=cl
+export PKG_CONFIG_PATH=$ROOT_DIR/installed/lib/pkgconfig
 
-# cd $ROOT_DIR/build
-# mkdir -p ffmpeg
-# cd ffmpeg
+./../../sources/ffmpeg/configure \
+             --prefix=./../../installed \
+             --toolchain=msvc \
+             --arch=x86_64 \
+             --enable-x86asm \
+             --enable-asm \
+             --enable-shared \
+             --disable-static \
+             --disable-programs \
+             --enable-swresample \
+             --enable-libx264 \
+             --enable-gpl \
+             --extra-ldflags="-LIBPATH:./../../installed/lib/" \
+             --extra-cflags="-I./../../installed/include/"
 
-# export CC=cl
-# export PKG_CONFIG_PATH=$ROOT_DIR/installed/lib/pkgconfig
+make -j ${CPU_CORES}
+make install
 
-# ./../../sources/ffmpeg/configure \
-#              --prefix=./../../installed \
-#              --toolchain=msvc \
-#              --arch=x86_64 \
-#              --enable-x86asm \
-#              --enable-asm \
-#              --enable-shared \
-#              --disable-static \
-#              --disable-programs \
-#              --enable-swresample \
-#              --enable-libx264 \
-#              --enable-gpl \
-#              --extra-ldflags="-LIBPATH:./../../installed/lib/" \
-#              --extra-cflags="-I./../../installed/include/"
-
-# make -j ${CPU_CORES}
-# make install
-
-# #
-# # Built done
-# #
-# echo "GPL ffmpeg built done."
-# echo "Set the environment varialbe FFMPEG_ROOT=$ROOT_DIR/installed"
+#
+# Built done
+#
+echo "GPL ffmpeg built done."
+echo "Set the environment varialbe FFMPEG_ROOT=$ROOT_DIR/installed"
 
