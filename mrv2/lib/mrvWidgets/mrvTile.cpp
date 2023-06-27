dacdbda10da3f59cd068346cf48c61c4ecc76fb7
Gonzalo Garramuño <ggarra13@gmail.com>
2023-06-26 23:01:03 -0300

    Work-around for Windows Fl_Tile bug.

--- mrv2/lib/mrvWidgets/mrvTile.cpp
+++ mrv2/lib/mrvWidgets/mrvTile.cpp
@@ -68,14 +68,47 @@
 */
 
 #include <iostream>
+#include <vector>
 
 #include <mrvWidgets/mrvTile.h>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Rect.H>
+#include <FL/Fl.H>
 #include <stdlib.h>
 
 namespace mrv
 {
+    struct WidgetData
+    {
+        Fl_Widget* o;
+        int X;
+        int Y;
+        int W;
+        int H;
+    };
+
+    struct MoveData
+    {
+        Tile* t;
+        std::vector<WidgetData> widgets;
+    };
+
+    static void move_cb(void* data)
+    {
+        MoveData* d = static_cast<MoveData*>(data);
+
+        for (const auto& w : d->widgets)
+        {
+            Fl_Widget* o = w.o;
+            int X = w.X;
+            int Y = w.Y;
+            int W = w.W;
+            int H = w.H;
+            o->damage_resize(X, Y, W, H);
+        }
+        d->t->init_sizes();
+        delete d;
+    }
 
     /**
       Drags the intersection at (\p oldx,\p oldy) to (\p newx,\p newy).
@@ -88,6 +121,8 @@ namespace mrv
         Fl_Widget* const* a = array();
         Fl_Rect* p = bounds();
         p += 2; // skip group & resizable's saved size
+        MoveData* data = new MoveData;
+        data->t = this;
         for (int i = children(); i--; p++)
         {
             Fl_Widget* o = *a++;
@@ -119,8 +154,15 @@ namespace mrv
                     (t < oldy && B > newy))
                     B = newy;
             }
-            o->damage_resize(X, Y, R - X, B - Y);
+            WidgetData widget;
+            widget.o = o;
+            widget.X = X;
+            widget.Y = Y;
+            widget.W = R - X;
+            widget.H = B - Y;
+            data->widgets.push_back(widget);
         }
+        Fl::add_timeout(0.0, (Fl_Timeout_Handler)move_cb, data);
     }
 
     void Tile::resize(int X, int Y, int W, int H)
@@ -134,27 +176,169 @@ namespace mrv
         Fl_Tile::init_sizes();
     }
 
+    static void set_cursor(Fl_Tile* t, Fl_Cursor c)
+    {
+        static Fl_Cursor cursor;
+        if (cursor == c || !t->window())
+            return;
+        cursor = c;
+#ifdef __sgi
+        t->window()->cursor(c, FL_RED, FL_WHITE);
+#else
+        t->window()->cursor(c);
+#endif
+    }
+
+    static Fl_Cursor cursors[4] = {
+        FL_CURSOR_DEFAULT, FL_CURSOR_WE, FL_CURSOR_NS, FL_CURSOR_MOVE};
+
     int Tile::handle(int event)
     {
+#ifdef _WIN32
+        static int sdrag;
+        static int sdx, sdy;
+        static int sx, sy;
+#    define DRAGH 1
+#    define DRAGV 2
+#    define GRABAREA 4
+
+        int mx = Fl::event_x();
+        int my = Fl::event_y();
+
+        switch (event)
+        {
+
+        case FL_MOVE:
+        case FL_ENTER:
+        case FL_PUSH:
+            // don't potentially change the mouse cursor if inactive:
+            if (!active())
+                break; // will cascade inherited handle()
+            {
+                int mindx = 100;
+                int mindy = 100;
+                int oldx = 0;
+                int oldy = 0;
+                Fl_Widget* const* a = array();
+                Fl_Rect* q = bounds();
+                Fl_Rect* p = q + 2;
+                for (int i = children(); i--; p++)
+                {
+                    Fl_Widget* o = *a++;
+                    if (o == resizable())
+                        continue;
+                    if (p->r() < q->r() && o->y() <= my + GRABAREA &&
+                        o->y() + o->h() >= my - GRABAREA)
+                    {
+                        int t = mx - (o->x() + o->w());
+                        if (abs(t) < mindx)
+                        {
+                            sdx = t;
+                            mindx = abs(t);
+                            oldx = p->r();
+                        }
+                    }
+                    if (p->b() < q->b() && o->x() <= mx + GRABAREA &&
+                        o->x() + o->w() >= mx - GRABAREA)
+                    {
+                        int t = my - (o->y() + o->h());
+                        if (abs(t) < mindy)
+                        {
+                            sdy = t;
+                            mindy = abs(t);
+                            oldy = p->b();
+                        }
+                    }
+                }
+                sdrag = 0;
+                sx = sy = 0;
+                if (mindx <= GRABAREA)
+                {
+                    sdrag = DRAGH;
+                    sx = oldx;
+                }
+                if (mindy <= GRABAREA)
+                {
+                    sdrag |= DRAGV;
+                    sy = oldy;
+                }
+                set_cursor(this, cursors[sdrag]);
+                if (sdrag)
+                    return 1;
+                return Fl_Group::handle(event);
+            }
+
+        case FL_LEAVE:
+            set_cursor(this, FL_CURSOR_DEFAULT);
+            break;
+
+        case FL_DRAG:
+            // This is necessary if CONSOLIDATE_MOTION in Fl_x.cxx is turned
+            // off: if (damage()) return 1; // don't fall behind
+        case FL_RELEASE:
+        {
+            if (!sdrag)
+                break;
+            Fl_Widget* r = resizable();
+            if (!r)
+                r = this;
+            int newx;
+            if (sdrag & DRAGH)
+            {
+                newx = Fl::event_x() - sdx;
+                if (newx < r->x())
+                    newx = r->x();
+                else if (newx > r->x() + r->w())
+                    newx = r->x() + r->w();
+            }
+            else
+                newx = sx;
+            int newy;
+            if (sdrag & DRAGV)
+            {
+                newy = Fl::event_y() - sdy;
+                if (newy < r->y())
+                    newy = r->y();
+                else if (newy > r->y() + r->h())
+                    newy = r->y() + r->h();
+            }
+            else
+                newy = sy;
+            move_intersection(sx, sy, newx, newy);
+            if (event == FL_DRAG)
+            {
+                set_changed();
+                do_callback(FL_REASON_DRAGGED);
+            }
+            else
+            {
+                do_callback(FL_REASON_CHANGED);
+            }
+            return 1;
+        }
+        }
+        return Fl_Group::handle(event);
+#else
         int ret = Fl_Tile::handle(event);
         if (event == FL_RELEASE)
             init_sizes();
         return ret;
+#endif
     }
 
     /**
-      Creates a new Tile widget using the given position, size,
-      and label string. The default boxtype is FL_NO_BOX.
+  Creates a new Tile widget using the given position, size,
+  and label string. The default boxtype is FL_NO_BOX.
 
-      The destructor <I>also deletes all the children</I>. This allows a
-      whole tree to be deleted at once, without having to keep a pointer to
-      all the children in the user code. A kludge has been done so the
-      Tile and all of its children can be automatic (local)
-      variables, but you must declare the Tile <I>first</I>, so
-      that it is destroyed last.
+  The destructor <I>also deletes all the children</I>. This allows a
+  whole tree to be deleted at once, without having to keep a pointer to
+  all the children in the user code. A kludge has been done so the
+  Tile and all of its children can be automatic (local)
+  variables, but you must declare the Tile <I>first</I>, so
+  that it is destroyed last.
 
-      \see class Fl_Group
-    */
+  \see class Fl_Group
+*/
 
     Tile::Tile(int X, int Y, int W, int H, const char* L) :
         Fl_Tile(X, Y, W, H, L)
