2af46287b2cb0ae449d939bfe5e2f283c6dcb7ca
Gonzalo Garramuño <ggarra13@gmail.com>
2023-07-13 17:47:04 -0300

    Documented mrv2.  Removed bin/sphinx_installs.sh which was incrorrect.

--- etc/sphinx_install.sh
+++ etc/sphinx_install.sh
@@ -8,4 +8,5 @@
 #
 
 export PYTHON=`ls $BUILD_DIR/install/bin/python* | grep -o 'python.*' | head -1 `
-run_cmd $BUILD_DIR/install/bin/$PYTHON -m pip install sphinx sphinx_rtd_theme
+run_cmd $BUILD_DIR/install/bin/$PYTHON -m pip install --upgrade pip
+run_cmd $BUILD_DIR/install/bin/$PYTHON -m pip install sphinx sphinx_rtd_theme requests urllib3
--- mrv2/docs/es/_static/documentation_options.js
+++ mrv2/docs/es/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.6.3',
+    VERSION: 'v0.7.0',
     LANGUAGE: 'es',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
