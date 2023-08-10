a5830404c9f38721b3830574e69c9078a9fd7e16
Gonzalo Garramuño <ggarra13@gmail.com>
2023-08-10 05:41:52 -0300

    Refactor.

---
+++ mrv2/lib/mrvApp/mrvUSD.h
@@ -0,0 +1,14 @@
+
+#include <tlIO/USD.h>
+
+namespace mrv
+{
+    namespace usd
+    {
+        //! Return the current USD Render Options
+        tl::usd::RenderOptions renderOptions();
+
+        //! Set the current USD Render Options
+        bool setRenderOptions(const tl::usd::RenderOptions& o);
+    } // namespace usd
+} // namespace mrv
--- mrv2/lib/mrvPy/USD.cpp
+++ mrv2/lib/mrvPy/USD.cpp
@@ -8,52 +8,9 @@
 
 namespace py = pybind11;
 
-#include <tlIO/USD.h>
-
 #include "mrvCore/mrvI8N.h"
 
-#include "mrvApp/mrvSettingsObject.h"
-#include "mrvApp/App.h"
-
-namespace mrv
-{
-    namespace usd
-    {
-        tl::usd::RenderOptions renderOptions()
-        {
-            App* app = App::application();
-            auto settingsObject = app->settingsObject();
-            tl::usd::RenderOptions o;
-            o.renderWidth =
-                std_any_cast<int>(settingsObject->value("usd/renderWidth"));
-            o.complexity =
-                std_any_cast<float>(settingsObject->value("usd/complexity"));
-            o.drawMode = static_cast<tl::usd::DrawMode>(
-                std_any_cast<int>(settingsObject->value("usd/drawMode")));
-            o.enableLighting = static_cast<bool>(
-                std_any_cast<int>(settingsObject->value("usd/enableLighting")));
-            o.stageCacheCount =
-                std_any_cast<int>(settingsObject->value("usd/stageCacheCount"));
-            o.diskCacheByteCount = std_any_cast<int>(
-                settingsObject->value("usd/diskCacheByteCount"));
-            return o;
-        }
-
-        bool setRenderOptions(const tl::usd::RenderOptions& o)
-        {
-            App* app = App::application();
-            auto settingsObject = app->settingsObject();
-            settingsObject->setValue("usd/renderWidth", o.renderWidth);
-            settingsObject->setValue("usd/complexity", o.complexity);
-            settingsObject->setValue("usd/drawMode", o.drawMode);
-            settingsObject->setValue("usd/enableLighting", o.enableLighting);
-            settingsObject->setValue("usd/stageCacheCount", o.stageCacheCount);
-            settingsObject->setValue(
-                "usd/diskCacheByteCount", o.diskCacheByteCount);
-            return true;
-        }
-    } // namespace usd
-} // namespace mrv
+#include "mrvApp/mrvUSD.h"
 
 void mrv2_usd(pybind11::module& m)
 {
