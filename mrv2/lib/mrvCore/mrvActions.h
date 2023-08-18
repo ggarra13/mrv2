0ec9aff0543484dcad672863b704090f01f7dfe3
Gonzalo Garramuño <ggarra13@gmail.com>
2023-08-18 20:56:47 -0300

    Refactor ActionMode and added Edit modes.

---
+++ mrv2/lib/mrvCore/mrvActions.h
@@ -0,0 +1,26 @@
+
+#pragma once
+
+namespace mrv
+{
+    enum ActionMode {
+        kScrub,
+        kSelection,
+        kDraw,
+        kErase,
+        kCircle,
+        kRectangle,
+        kArrow,
+        kText,
+        kRotate,
+        kEditOverwrite,
+        kEditInsert,
+        kEditTrim,
+        kEditSlice,
+        kEditSlip,
+        kEditSlide,
+        kEditRipple,
+        kEditRoll,
+        kEditFill
+    };
+}
