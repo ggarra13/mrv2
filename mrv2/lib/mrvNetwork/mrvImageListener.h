325f8276c82feb24193e5a4679083fe69cb2b4ae
Gonzalo Garramuño <ggarra13@gmail.com>
2023-09-30 00:44:32 -0300

    Added missing files.

---
+++ mrv2/lib/mrvNetwork/mrvImageListener.h
@@ -0,0 +1,44 @@
+
+#pragma once
+
+#include <tlCore/Util.h>
+
+#include <Poco/Net/ServerSocket.h>
+#include <Poco/Event.h> // For signaling the thread to exit
+
+namespace mrv
+{
+    class App;
+
+    const int kPORT_NUMBER = 55120;
+
+    class ImageSender
+    {
+    public:
+        ImageSender(uint16_t port = kPORT_NUMBER);
+
+        bool isRunning();
+        void sendImage(const std::string& imageData);
+
+    private:
+        Poco::Net::SocketAddress address;
+        Poco::Net::StreamSocket socket;
+    };
+
+    bool isInstanceRunning();
+
+    class ImageListener : public Poco::Runnable
+    {
+    public:
+        ImageListener(App* app, uint16_t port = kPORT_NUMBER);
+
+        void run();
+        void stop();
+
+    private:
+        App* app_ = nullptr;
+        Poco::Net::ServerSocket serverSocket;
+        std::thread* acceptThread = nullptr;
+        bool running = false;
+    };
+} // namespace mrv
