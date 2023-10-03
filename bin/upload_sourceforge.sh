07f1a02cb7ccde77d87e2e833a0f8e20fa54901c
Gonzalo Garramuño <ggarra13@gmail.com>
2023-10-03 12:28:04 -0300

    Added Donate.

--- bin/upload_sourceforge.sh
+++ bin/upload_sourceforge.sh
@@ -96,6 +96,8 @@ export LC_ALL=$oldALL
 #
 cat <<EOF > README.md
 
+[![Donate](https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=679N8GWCNDFSJ)
+
 mrv2 v${mrv2_VERSION} Beta
 ================
 
