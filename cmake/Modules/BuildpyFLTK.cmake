46c1d032c76a255eb76c74746c728dfad54aff41
Gonzalo Garramuño <ggarra13@gmail.com>
2023-10-04 21:35:26 -0300

    Made it work on Windows.

--- cmake/Modules/BuildpyFLTK.cmake
+++ cmake/Modules/BuildpyFLTK.cmake
@@ -15,13 +15,14 @@ if(NOT PYTHON_EXECUTABLE)
     endif()
 endif()
 
-message(STATUS "pyFLTK got PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}" )
 
-set(pyFLTK_PATCH ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_SOURCE_DIR}/cmake/patches/pyFLTK-patch/swig/x.i" "${CMAKE_BINARY_DIR}/pyFLTK-prefix/src/pyFLTK/swig/"
-    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PROJECT_SOURCE_DIR}/cmake/patches/pyFLTK-patch/setup.py" "${CMAKE_BINARY_DIR}/pyFLTK-prefix/src/pyFLTK/")
-set(pyFLTK_CONFIGURE ${PYTHON_EXECUTABLE} setup.py swig)
-set(pyFLTK_BUILD     ${PYTHON_EXECUTABLE} setup.py build)
-set(pyFLTK_INSTALL   ${PYTHON_EXECUTABLE} setup.py install)
+set(pyFLTK_PATCH
+    ${CMAKE_COMMAND} -E copy_if_different
+    "${PROJECT_SOURCE_DIR}/cmake/patches/pyFLTK-patch/swig/x.i"
+    "${CMAKE_BINARY_DIR}/pyFLTK-prefix/src/pyFLTK/swig/")
+set(pyFLTK_CONFIGURE ${CMAKE_COMMAND} -E env FLTK_HOME=${FLTK_HOME} ${PYTHON_EXECUTABLE} setup.py swig)
+set(pyFLTK_BUILD     ${CMAKE_COMMAND} -E env FLTK_HOME=${FLTK_HOME} ${PYTHON_EXECUTABLE} setup.py build)
+set(pyFLTK_INSTALL   ${CMAKE_COMMAND} -E env FLTK_HOME=${FLTK_HOME} ${PYTHON_EXECUTABLE} setup.py install)
 
 ExternalProject_Add(
     pyFLTK
