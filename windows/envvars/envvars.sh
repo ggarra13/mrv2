61be1857d7041e6a9aeb9735e2475213ea3580f4
Gonzalo Garramuño <ggarra13@gmail.com>
2023-10-06 08:47:24 -0300

    Moved precompiled DLLs.

--- windows/envvars/envvars.sh
+++ windows/envvars/envvars.sh
@@ -16,8 +16,12 @@
 #
 # On Linux and macOS this is always compiled by cmake.
 #
-
 export LCMS2_ROOT=$PWD/precompiled/windows/win64/
+if [[ ! -e $LCMS2_ROOT ]]; then
+    echo "LCMS2_ROOT=$LCMS2_ROOT"
+    echo "Not found in the repository."
+    exit 1
+fi
 
 #
 # Set the location of precompiled FFmpeg on Windows.
