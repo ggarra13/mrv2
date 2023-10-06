ee1ef64d0739e68bcd1fa307b467d19ee1625a42
Gonzalo Garramuño <ggarra13@gmail.com>
2023-10-05 21:28:36 -0300

    Updated build.

--- cmake/Modules/BuildGettext.cmake
+++ cmake/Modules/BuildGettext.cmake
@@ -6,38 +6,42 @@ include( ExternalProject )
 
 
 if(APPLE)
-    set(GETTEXT_CXX_FLAGS "-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET} ${CMAKE_CXX_FLAGS}")
-    set(GETTEXT_C_FLAGS "-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET} ${CMAKE_C_FLAGS}")
+    set(GETTEXT_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+    set(GETTEXT_C_FLAGS "${CMAKE_C_FLAGS}")
+    if (CMAKE_OSX_DEPLOYMENT_TARGET)
+	list(APPEND GETTEXT_CXX_FLAGS "-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+	list(APPEND GETTEXT_C_FLAGS "-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    endif()
 endif()
 
 set(GETTEXT_ARGS
-	--enable-relocatable
-	--disable-java
-	--disable-static
-	--enable-shared
-	--disable-curses
+    --enable-relocatable
+    --disable-java
+    --disable-static
+    --enable-shared
+    --disable-curses
 
-	--disable-c++
-	--disable-curses
-	--disable-nls
-	--disable-acl
-	--disable-dependency-tracking
-	--disable-libaseprintf
-	--disable-openmp
-	--disable-xattr
-	--without-libtextstyle-prefix
-	--without-emacs
-	--without-bzip2
-	--without-xz
-	--prefix=${CMAKE_INSTALL_PREFIX})
+    --disable-c++
+    --disable-curses
+    --disable-nls
+    --disable-acl
+    --disable-dependency-tracking
+    --disable-libaseprintf
+    --disable-openmp
+    --disable-xattr
+    --without-libtextstyle-prefix
+    --without-emacs
+    --without-bzip2
+    --without-xz
     
+    --prefix=${CMAKE_INSTALL_PREFIX})
+
 ExternalProject_Add(
     GETTEXT
     URL "https://ftp.gnu.org/gnu/gettext/gettext-0.22.3.tar.gz"
     CONFIGURE_COMMAND ./configure ${GETTEXT_ARGS}
     BUILD_COMMAND     make libintl.dylib
     "CFLAGS=${GETTEXT_C_FLAGS}"
-    "CPPFLAGS=${GETTEXT_C_FLAGS}"
     "CXXFLAGS=${GETTEXT_CXX_FLAGS}"
     BUILD_IN_SOURCE 1
 )
