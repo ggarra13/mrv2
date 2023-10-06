6fc07cf0dfb3644a5ec4fd98932771d066e443b3
Gonzalo Garramuño <ggarra13@gmail.com>
2023-10-06 18:00:04 -0300

    Made mrv2 depend on pyFLTK so that it can remove the tests directory from the distribution.

--- cmake/Modules/Buildmrv2.cmake
+++ cmake/Modules/Buildmrv2.cmake
@@ -47,7 +47,7 @@ set(mrv2_ARGS
 ExternalProject_Add(
     mrv2
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/mrv2
-    DEPENDS tlRender ${FLTK_DEP} ${PYBIND11_DEP} ${POCO_DEP} ${LibHaru_DEP} ${Gettext} ${ZLIB_SHARED}
+    DEPENDS tlRender ${FLTK_DEP} ${PYBIND11_DEP} ${pyFLTK_DEP} ${POCO_DEP} ${LibHaru_DEP} ${Gettext}
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/mrv2
     LIST_SEPARATOR |
     CMAKE_ARGS ${mrv2_ARGS}
