cd5d96c0698107b3d7061953df7fd3525bda1e1f
Gonzalo Garramuño <ggarra13@gmail.com>
2023-10-13 03:01:20 -0300

    Added missing mrvIntInput.h.

---
+++ mrv2/lib/mrvWidgets/mrvIntInput.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <FL/Fl_Int_Input.H>
+
+namespace mrv
+{
+    class IntInput : public Fl_Int_Input
+    {
+    public:
+        IntInput(int X, int Y, int W, int H, const char* L = 0) :
+            Fl_Int_Input(X, Y, W, H, L)
+        {
+            labelsize(12);
+            color((Fl_Color)0xf98a8a800);
+            textcolor(FL_BLACK);
+            cursor_color(FL_RED);
+        }
+    };
+} // namespace mrv
