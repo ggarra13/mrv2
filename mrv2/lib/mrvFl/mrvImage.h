d4bc55a8c019d24f78a18b8f1ddaa838563bb881
Gonzalo Garramuño <ggarra13@gmail.com>
2023-11-16 18:50:30 -0300

    Made viewport and timeline panning work with CTRL + LMB, too.
    Added Display / View Preferences to OCIO Defaults.
    
    Fixed setOcioView() and ocioView().
    
    Added ocioViewList().

---
+++ mrv2/lib/mrvFl/mrvImage.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace mrv
+{
+    namespace image
+    {
+
+        std::string ocioView();
+
+        void setOcioView(const std::string&);
+    } // namespace image
+} // namespace mrv
