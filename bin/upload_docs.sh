f5e8e166d21b6688ac959debb63328e045f06e06
Gonzalo Garramuño <ggarra13@gmail.com>
2023-12-08 03:49:49 -0300

    Added license information.

--- bin/upload_docs.sh
+++ bin/upload_docs.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
 #!/bin/bash
 
 rsync -avP --exclude '*~' -e ssh docs/www/* ggarra13@web.sourceforge.net:/home/project-web/mrv2/htdocs
