a845a474f794a1035c114dd104c92d1b215d4245
Gonzalo Garramuño <ggarra13@gmail.com>
2024-01-18 20:34:19 -0300

    Simplified runme_ndi.sh script.

--- bin/runme_ndi.sh
+++ bin/runme_ndi.sh
@@ -5,40 +5,13 @@
 
 
 #
+# Minimal build script for mrv2 with NDI support.
 #
-# Minimal build script for mrv2.  It builds all dependencies and will install
-# the main executable on BUILD_DIR (by default
-#                                   BUILD-OS-ARCH/BUILD_TYPE/install/bin).
-#
-# On Linux and macOS, it will also create a mrv2 or mrv2-dbg symbolic link
-# in $HOME/bin if the directory exists.
-#
-# It will also log the compilation on $BUILD_DIR/compile.log
-#
-
 
 #
 # Store the parameters for passing them later
 #
 params="$@"
-
-#
-# Find out our build dir
-#
-. etc/build_dir.sh
-
-mkdir -p $BUILD_DIR
-
-
-#
-# Clear the flags, as they will be set by runme_nolog.sh.
-#
-export FLAGS=""
-export CMAKE_FLAGS=""
-
-
-echo
-echo "Saving compile log to $BUILD_DIR/compile.log ..."
 
 export TLRENDER_NDI=ON
 
@@ -50,5 +23,5 @@ else
     export TLRENDER_NDI_SDK="/Library/NDI\ SDK\ for\ Apple/"
 fi
 
-cmd="./runme_nolog.sh $params 2>&1 | tee $BUILD_DIR/compile.log"
+cmd="./runme.sh $params"
 run_cmd $cmd
