40c656726a1d2e93ed23fe1e14a42ee4c0279f82
Gonzalo Garramuño <ggarra13@gmail.com>
2024-02-24 21:55:46 -0300

    Faster windows minimal compile.

--- bin/compile_liblcms2_windows.sh
+++ bin/compile_liblcms2_windows.sh
@@ -32,60 +32,66 @@ if [[ $KERNEL != *Msys* ]]; then
     exit 1
 fi
 
+if [[ -z $TLRENDER_RAW ]]; then
+    export TLRENDER_RAW=ON
+fi
+
+
 MRV2_ROOT=$PWD
 SUPERBUILD=$PWD/$BUILD_DIR/tlRender/etc/SuperBuild
 INSTALLDIR=$PWD/$BUILD_DIR/install
 
-if [[ ! -e $INSTALLDIR/lib/liblcms2.lib ]]; then
+if [[ $TLRENDER_RAW == ON || $TLRENDER_RAW == 1 ]]; then
+    if [[ ! -e $INSTALLDIR/lib/liblcms2.lib ]]; then
 
-    #
-    # Install development tools
-    #
-    pacman -Sy --noconfirm
+	#
+	# Install development tools
+	#
+	pacman -Sy --noconfirm
 
-    pacman -Sy binutils --noconfirm
+	pacman -Sy binutils --noconfirm
     
-    #
-    # Clone the repository
-    #
-    mkdir -p $SUPERBUILD
-    cd $SUPERBUILD
-    if [[ ! -d LCMS2 ]]; then
-	run_cmd git clone --depth 1 --branch $LCMS_BRANCH https://github.com/mm2/Little-CMS.git LCMS2 2> /dev/null
+	#
+	# Clone the repository
+	#
+	mkdir -p $SUPERBUILD
+	cd $SUPERBUILD
+	if [[ ! -d LCMS2 ]]; then
+	    run_cmd git clone --depth 1 --branch $LCMS_BRANCH https://github.com/mm2/Little-CMS.git LCMS2 2> /dev/null
+	fi
+    
+	#
+	# Run configure
+	#
+	cd LCMS2
+	
+	export CC=cl
+	export CXX=cl
+	export LD=link
+
+	#
+	# Remove some cl.exe compile warnings
+	#
+	sed -i -e 's#-fvisibility=hidden##' ./configure
+	sed -i -e 's#CFLAGS="-g"#CFLAGS=""#' ./configure
+	sed -i -e 's#CXXFLAGS="-g"#CXXFLAGS=""#' ./configure
+	
+	run_cmd ./configure --build=mingw64 --enable-shared --disable-static --prefix=$INSTALLDIR
+    
+	#
+	# Compile and install the library
+	#
+	make -j ${CPU_CORES} install
+
+	echo "Compiled result:"
+	echo
+	ls $INSTALLDIR/lib/*lcms2*
+	echo
+    
+	run_cmd mv $INSTALLDIR/lib/lcms2.dll.lib $INSTALLDIR/lib/liblcms2.lib
+    
+	cd $MRV2_ROOT
+    else
+	echo "liblcms2 already installed."
     fi
-    
-    #
-    # Run configure
-    #
-    cd LCMS2
-    
-    export CC=cl
-    export CXX=cl
-    export LD=link
-
-    #
-    # Remove some cl.exe compile warnings
-    #
-    sed -i -e 's#-fvisibility=hidden##' ./configure
-    sed -i -e 's#CFLAGS="-g"#CFLAGS=""#' ./configure
-    sed -i -e 's#CXXFLAGS="-g"#CXXFLAGS=""#' ./configure
-    
-    run_cmd ./configure --build=mingw64 --enable-shared --disable-static --prefix=$INSTALLDIR
-    
-    #
-    # Compile and install the library
-    #
-    make -j ${CPU_CORES} install
-
-    echo "Compiled result:"
-    echo
-    ls $INSTALLDIR/lib/*lcms2*
-    echo
-    
-    run_cmd mv $INSTALLDIR/lib/lcms2.dll.lib $INSTALLDIR/lib/liblcms2.lib
-    
-    cd $MRV2_ROOT
-else
-    echo "liblcms2 already installed."
 fi
-
