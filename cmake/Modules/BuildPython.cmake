06f4f2e8ebb4465b2f5f4c5c9a09981b4f9ca799
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-10 14:36:41 -0300

    Build python fix.

--- cmake/Modules/BuildPython.cmake
+++ cmake/Modules/BuildPython.cmake
@@ -30,7 +30,7 @@ if(APPLE)
     set( Python_CONFIGURE ${CMAKE_COMMAND} -E env "CFLAGS=${Python_C_FLAGS}" "CPPFLAGS=${Python_C_FLAGS}" "CXXFLAGS=${Python_CXX_FLAGS}" "LDFLAGS=${Python_LD_FLAGS}" -- ${CMAKE_CURRENT_BINARY_DIR}/configure-macos-python.sh --prefix=${CMAKE_INSTALL_PREFIX}
     )
     set( Python_BUILD    )
-    set( Python_INSTALL  make -j ${NPROCS} altinstall )
+    set( Python_INSTALL  make -j ${NPROCS} install )
 
 elseif(UNIX)
 
@@ -43,8 +43,8 @@ elseif(UNIX)
 	--enable-shared
         --prefix=${CMAKE_INSTALL_PREFIX}
     )
-    set( Python_BUILD    )
-    set( Python_INSTALL  make -j ${NPROCS} altinstall )
+    set( Python_BUILD   make -j ${NPROCS} )
+    set( Python_INSTALL  make -j ${NPROCS} install )
 else()
 
     set( platform x64 )
