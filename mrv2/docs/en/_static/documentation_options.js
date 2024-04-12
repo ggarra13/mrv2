a0a5b0354b88cb52cfbcfdbf58e23c25138df43d
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-12 18:33:21 -0300

    Updated BuildpyFLTK.cmake.

--- mrv2/docs/en/_static/documentation_options.js
+++ mrv2/docs/en/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v1.1.1',
+    VERSION: 'v1.1.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- mrv2/docs/es/_static/documentation_options.js
+++ mrv2/docs/es/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v1.1.1',
+    VERSION: 'v1.1.2',
     LANGUAGE: 'es',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
