4684e813caf0fa4bac74bbd8bcb19b8903f17abe
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-13 23:33:04 -0300

    Updated docs.

--- mrv2/docs/en/_static/documentation_options.js
+++ mrv2/docs/en/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v1.1.3',
+    VERSION: 'v1.1.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- mrv2/docs/es/_static/documentation_options.js
+++ mrv2/docs/es/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v1.1.3',
+    VERSION: 'v1.1.2',
     LANGUAGE: 'es',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
