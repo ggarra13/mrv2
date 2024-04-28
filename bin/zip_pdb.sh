66c0bd78add85e12ca7d836c4a11de06df68b9d6
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-28 05:52:05 -0300

    Fixed name of zip_pdb's filename.

--- bin/zip_pdb.sh
+++ bin/zip_pdb.sh
@@ -6,6 +6,6 @@
 # Output zip file name
 ZIPFILE="mrv2_pdb_files.zip"
 
-find "$BUILD_DIR" -type f -name "*.pdb" -exec zip output_pdb_files.zip {} +
+find "$BUILD_DIR" -type f -name "*.pdb" -exec zip $ZIPFILE {} +
 
 echo "PDB files and directories zipped into $ZIPFILE"
