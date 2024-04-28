c59eff14ac35326393a364d79e17a5eddf9ebf24
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-28 00:18:16 -0300

    Added documentation to zip_pdb.sh.

--- bin/zip_pdb.sh
+++ bin/zip_pdb.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+#
+# This Windows script collects all .pdb files of a Debug or RelWithDebInfo
+# into a zipfile.
+#
+
+
 
 . etc/build_dir.sh
 
@@ -6,6 +16,6 @@
 # Output zip file name
 ZIPFILE="mrv2_pdb_files.zip"
 
-find "$BUILD_DIR" -type f -name "*.pdb" -exec zip output_pdb_files.zip {} +
+find "$BUILD_DIR" -type f -name "*.pdb" -exec zip $ZIPFILE {} +
 
-echo "PDB files and directories zipped into $ZIPFILE"
+echo "PDB files from $BUILD_DIR zipped into $ZIPFILE"
