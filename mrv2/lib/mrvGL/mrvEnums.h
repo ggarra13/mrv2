d1fbecef09a8c325928506d665d67b1df32de7ee
Gonzalo Garramuño <ggarra13@gmail.com>
2024-04-29 11:35:47 -0300

    Refactored enums.

---
+++ mrv2/lib/mrvGL/mrvEnums.h
@@ -0,0 +1,37 @@
+
+namespace mrv
+{
+
+    enum ColorAccuracy {
+        kAccuracyAuto,
+        kAccuracyFloat16,
+        kAccuracyFloat32,
+        kAccuracyFast
+    };
+
+    enum MonitorVSync { kVSyncNone, kVSyncAlways, kVSyncPresentationOnly };
+
+    enum PixelDisplay { kRGBA_Float, kRGBA_Hex, kRGBA_Decimal };
+
+    enum PixelValue { kFull, kOriginal };
+
+    enum Blit { kNoBlit, kBlit };
+
+    enum HudDisplay {
+        kNone = 0,
+        kFilename = 1 << 0,
+        kDirectory = 1 << 1,
+        kFrame = 1 << 2,
+        kFrameRange = 1 << 3,
+        kFrameCount = 1 << 4,
+        kResolution = 1 << 5,
+        kTimecode = 1 << 6,
+        kFPS = 1 << 7,
+        kMemory = 1 << 8,
+        kCache = 1 << 9,
+        kAttributes = 1 << 10,
+    };
+
+    enum MissingFrameType { kBlackFrame, kRepeatFrame, kScratchedFrame };
+
+}
