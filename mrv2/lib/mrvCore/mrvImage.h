f6f755ecde3a5a8bcaa7f0b24cb8fb5cb88e3689
Gonzalo Garramuño <ggarra13@gmail.com>
2024-05-19 06:03:21 -0300

    Added search_string.sh script.
    Added licensing.

--- mrv2/lib/mrvCore/mrvImage.h
+++ mrv2/lib/mrvCore/mrvImage.h
@@ -1,12 +1,15 @@
-#pragma once
+// SPDX-License-Identifier: BSD-3-Clause
+// mrv2
+// Copyright Contributors to the mrv2 Project. All rights reserved.
 
+#pragma once
 
 namespace mrv
 {
-    
-    inline
-    void flipImageInY(uint8_t* pixels, const size_t width, const size_t height,
-                      const int depth)
+
+    inline void flipImageInY(
+        uint8_t* pixels, const size_t width, const size_t height,
+        const int depth)
     {
         const size_t rowSize = width * depth;
         for (size_t y = 0; y < height / 2; ++y)
@@ -23,23 +26,28 @@ namespace mrv
         }
     }
 
-    inline
-    void flipImageInY(uint8_t* outputPixels,
-                      const uint8_t* inputPixels, const size_t width, const size_t height, const int depth)
+    inline void flipImageInY(
+        uint8_t* outputPixels, const uint8_t* inputPixels, const size_t width,
+        const size_t height, const int depth)
     {
         // Calculate the size of a row of pixels (in bytes)
         size_t rowSize = width * depth;
 
         // Loop through half the image height
-        for (size_t y = 0; y < height ; ++y) {
-            // Calculate the indices of the rows in source and destination buffers
+        for (size_t y = 0; y < height; ++y)
+        {
+            // Calculate the indices of the rows in source and destination
+            // buffers
             size_t topInputRow = y * rowSize;
             size_t bottomInputRow = (height - y - 1) * rowSize;
-            size_t topOutputRow = (height - y - 1) * rowSize; // Start from bottom in output
+            size_t topOutputRow =
+                (height - y - 1) * rowSize; // Start from bottom in output
 
             // Copy a row from top to bottom in the output buffer
-            memcpy(outputPixels + topOutputRow, inputPixels + topInputRow, rowSize);
+            memcpy(
+                outputPixels + topOutputRow, inputPixels + topInputRow,
+                rowSize);
         }
     }
-    
-}
+
+} // namespace mrv
