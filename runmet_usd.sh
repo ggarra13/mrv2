a223074e319646481499b08f5067c6c860613f60
Gonzalo Garramuño <ggarra13@gmail.com>
2024-06-10 21:19:50 -0300

    Added runme*_usd.sh scripts.

---
+++ runmet_usd.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+
+#
+#
+# Main build script for mrv2.  It builds all dependencies and will install the
+# main executable on BUILD_DIR (by default
+#                               BUILD-OS-ARCH/BUILD_TYPE/install/bin).
+#
+# On Linux and macOS, it will also create a mrv2 or mrv2-dbg symbolic link
+# in $HOME/bin if the directory exists.
+#
+# It will also log the compilation on $BUILD_DIR/compile.log
+#
+
+
+#
+# Store the parameters for passing them later
+#
+params=$*
+
+#
+# Find out our build dir
+#
+. etc/build_dir.sh
+
+export BUILD_ROOT=BUILD-$KERNEL-$ARCH-usd
+
+cmd="runmet.sh --dir $BUILD_ROOT"
+run_cmd $cmd
