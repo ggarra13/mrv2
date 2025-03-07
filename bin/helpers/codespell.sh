f7615b8c2a47d63548dd4ee6a1b52d5f19ac3043
Gonzalo Garramuño <ggarra13@gmail.com>
2025-03-07 01:06:11 -0300

    Added codespell.sh script.

---
+++ bin/helpers/codespell.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+codespell -q 3 -S "*.po,*.pot,*.svg,./docs/sphinx/es,./src/docs/es,./src/docs/en/searchindex.js,./src/docs/HISTORY.md" -L aci,afile,childs,outlow,ot,substract,te,ue
