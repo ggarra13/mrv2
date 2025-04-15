b8f7be9c6ef222d1c1f0a6edba9324836c6f9ad7
Gonzalo Garramuño <ggarra13@gmail.com>
2025-04-15 18:45:41 -0300

    Refactor tlTimelineVk from tlUI when not compiled.

--- etc/runme_nolog.sh
+++ etc/runme_nolog.sh
@@ -303,9 +303,7 @@ echo "Build FLTK shared................... ${FLTK_BUILD_SHARED} 	(FLTK_BUILD_SHA
 echo "Build embedded Python............... ${MRV2_PYBIND11} 	(MRV2_PYBIND11)"
 echo "Build mrv2 Network connections...... ${MRV2_NETWORK} 	(MRV2_NETWORK)"
 echo "Build PDF........................... ${MRV2_PDF} 	(MRV2_PDF)"
-if [[ $MRV2_HDR == ON || $MRV2_HDR == 1 ]]; then
-    echo "MRV2_HDR............................ ${MRV2_HDR} 	(MRV2_HDR)"
-fi
+echo "Build hdr application............... ${MRV2_HDR} 	(MRV2_HDR)"
 echo
 echo "tlRender Options"
 echo
