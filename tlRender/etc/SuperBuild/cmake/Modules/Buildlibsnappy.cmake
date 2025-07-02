11cd0ab4ca7f1c1ab102d9f6ad0fd23dc06d5cf5
Gonzalo Garramuño <ggarra13@gmail.com>
2025-07-02 16:02:38 -0300

    libsnappy on Linux and macOS.

--- tlRender/etc/SuperBuild/cmake/Modules/Buildlibsnappy.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/Buildlibsnappy.cmake
@@ -7,15 +7,14 @@ set(libsnappy_TAG 1.2.2)
 
 set(libsnappy_ARGS ${TLRENDER_EXTERNAL_ARGS})
 
-
 if(APPLE)
-    set(libsnappy_CXX_COMPILER $ENV{NATIVE_COMPILER_NAME})
-    set(libsnappy_C_COMPILER $ENV{NATIVE_COMPILER_NAME})
+    set(libsnappy_CXX_COMPILER $ENV{NATIVE_COMPILER_NAME}) #not yet used
+    set(libsnappy_C_COMPILER $ENV{NATIVE_COMPILER_NAME}) # not yet used
     set(libsnappy_CXX_FLAGS -fPIC ${CMAKE_CXX_FLAGS})
     set(libsnappy_C_FLAGS -fPIC ${CMAKE_C_FLAGS})
 elseif (UNIX)
-    set(libsnappy_CXX_COMPILER $ENV{NATIVE_COMPILER_NAME})
-    set(libsnappy_C_COMPILER $ENV{NATIVE_COMPILER_NAME})
+    set(libsnappy_CXX_COMPILER $ENV{NATIVE_COMPILER_NAME}) # not yet used
+    set(libsnappy_C_COMPILER $ENV{NATIVE_COMPILER_NAME}) # not yet used
     set(libsnappy_CXX_FLAGS -fPIC ${CMAKE_CXX_FLAGS})
     set(libsnappy_C_FLAGS -fPIC ${CMAKE_C_FLAGS})
 else()
@@ -25,17 +24,25 @@ else()
     set(libsnappy_C_FLAGS "")
 endif()
 
+message(STATUS "libsnappy_CXX_COMPILER=${libsnappy_CXX_COMPILER}")
+message(STATUS "libsnappy_C_COMPILER=${libsnappy_C_COMPILER}")
+
+
 list(APPEND libsnappy_ARGS
     -DBENCHMARK_ENABLE_INSTALL=OFF
     -DBENCHMARK_INSTALL_DOCS=OFF
     -DBENCHMARK_USE_BUNDLED_GTEST=OFF
     -DSNAPPY_BUILD_TESTS=OFF
     -DSNAPPY_BUILD_BENCHMARKS=OFF
-    -DCMAKE_CXX_COMPILER="${libsnappy_CXX_COMPILER}"
-    -DCMAKE_C_COMPILER="${libsnappy_C_COMPILER}"
     -DCMAKE_CXX_FLAGS="${libsnappy_CXX_FLAGS}"
     -DCMAKE_C_FLAGS="${libsnappy_C_FLAGS}"
 )
+if(WIN32)
+    list(APPEND libsnappy_ARGS
+	-DCMAKE_CXX_COMPILER="${libsnappy_CXX_COMPILER}"
+	-DCMAKE_C_COMPILER="${libsnappy_C_COMPILER}"
+    )
+endif()
 
 set(libsnappy_UPDATE_CMD git submodule update --init)
 
