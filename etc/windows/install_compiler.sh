bef19423e19b1c5e148d7db9066c1efe81244f7b
Gonzalo Garramuño <ggarra13@gmail.com>
2025-07-02 18:01:49 -0300

    install_compiler.sh.

--- etc/windows/install_compiler.sh
+++ etc/windows/install_compiler.sh
@@ -2,6 +2,10 @@
 
 USE_MSVC=1
 if [[ $USE_MSVC == 1 ]]; then
-    pacman -R --noconfirm mingw-w64-x86_64-g++
-    pacman -R --noconfirm mingw-w64-x86_64-gcc
+    if [[ -e /usr/mingw64/g++.exe ]]; then
+	pacman -R --noconfirm mingw-w64-x86_64-g++
+    fi
+    if [[ -e /usr/mingw64/gcc.exe ]]; then
+	pacman -R --noconfirm mingw-w64-x86_64-gcc
+    fi
 fi
