39a5f6783f64a09fdd6e30a53da7be6eaeeb150a
Gonzalo Garramuño <ggarra13@gmail.com>
2025-07-02 18:42:09 -0300

    Updated compiler info.

--- etc/windows/install_compiler.sh
+++ etc/windows/install_compiler.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/bash
 
 USE_MSVC=1
-if [[ $USE_MSVC == 1 ]]; then
-    # if [[ -e /usr/mingw64/g++.exe ]]; then
-    # 	pacman -R --noconfirm mingw-w64-x86_64-g++
-    # fi
-    # if [[ -e /usr/mingw64/gcc.exe ]]; then
-    # 	pacman -R --noconfirm mingw-w64-x86_64-gcc
-    # fi
-fi
+USE_CLANG=1
