65e6983820420beadfb60581f78ea9b5fc930b7d
Gonzalo Garramuño <ggarra13@gmail.com>
2025-07-02 18:36:50 -0300

    generic compiler.

--- etc/windows/install_compiler.sh
+++ etc/windows/install_compiler.sh
@@ -2,10 +2,10 @@
 
 USE_MSVC=1
 if [[ $USE_MSVC == 1 ]]; then
-    if [[ -e /usr/mingw64/g++.exe ]]; then
-	pacman -R --noconfirm mingw-w64-x86_64-g++
-    fi
-    if [[ -e /usr/mingw64/gcc.exe ]]; then
-	pacman -R --noconfirm mingw-w64-x86_64-gcc
-    fi
+    # if [[ -e /usr/mingw64/g++.exe ]]; then
+    # 	pacman -R --noconfirm mingw-w64-x86_64-g++
+    # fi
+    # if [[ -e /usr/mingw64/gcc.exe ]]; then
+    # 	pacman -R --noconfirm mingw-w64-x86_64-gcc
+    # fi
 fi
