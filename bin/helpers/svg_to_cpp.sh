8a053807a35083364351a7e6e2deb44a7f33d024
Gonzalo Garramuño <ggarra13@gmail.com>
2025-07-24 18:48:44 -0300

    Added svg_to_cpp.py/.sh helper script.

---
+++ bin/helpers/svg_to_cpp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+
+#
+# This script checks the languages .po files for missing translations.
+#
+if [[ ! -e etc/build_dir.sh ]]; then
+    echo "You must run this script from the root of mrv2 directory like:"
+    echo
+    script=`basename $0`
+    echo "> bin/$script"
+    exit 1
+fi
+
+NOARGS=1
+. etc/build_dir.sh
+
+locate_python
+
+$PYTHON bin/python/svg_to_cpp.py $*
