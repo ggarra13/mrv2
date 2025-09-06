4b202294dde330dfd8510e98321e609e442de974
Gonzalo Garramuño <ggarra13@gmail.com>
2025-09-06 16:11:29 -0300

    Win32 Fix.

--- src/lib/mrvFl/mrvTimelinePlayer.h
+++ src/lib/mrvFl/mrvTimelinePlayer.h
@@ -33,7 +33,7 @@ namespace mrv
         virtual ~TimelinePlayer();
 
         //! Get the context.
-        const std::weak_ptr<system::Context>& context() const;
+        const std::weak_ptr<system::Context>& getContext() const;
 
         //! Get the timeline player.
         const std::shared_ptr<timeline::Player>& player() const;
@@ -355,7 +355,7 @@ namespace mrv
         //! Get list of annotations for between previous ghosting and
         //! next ghosting from current time.
         std::vector< std::shared_ptr< draw::Annotation >>
-        getAnnotations(const int, const int) const;
+        getAnnotations(const int from = 1, const int to = 1) const;
 
         //! Get all annotations in timeline player.
         std::vector< std::shared_ptr< draw::Annotation >>
@@ -383,12 +383,13 @@ namespace mrv
 
         //! Create annotation for current time
         std::shared_ptr< voice::Annotation >
-        createVoiceAnnotation(const bool all_frames = false);
+        createVoiceAnnotation(const math::Vector2i& center,
+                              const bool all_frames = false);
 
         //! Get list of annotations for between previous ghosting and
         //! next ghosting from current time.
         std::vector< std::shared_ptr< voice::Annotation >>
-        getVoiceAnnotations(const int, const int) const;
+        getVoiceAnnotations(const int from = 1, const int to = 1) const;
 
         //! Get all annotations in timeline player.
         std::vector< std::shared_ptr< voice::Annotation >>
