0ee6c95ba0beb3a98535395907d7896a9067d2e7
Gonzalo Garramuño <ggarra13@gmail.com>
2025-09-14 09:37:25 -0300

    Added building shaderc on Linux.

---
+++ cmake/Modules/Buildshaderc.cmake
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2 
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+include(ExternalProject)
+
+set(shaderc_ARGS
+    ${TLRENDER_EXTERNAL_ARGS}
+)
+
+ExternalProject_Add(
+    shaderc
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/shaderc
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/src
+    LIST_SEPARATOR |
+    CMAKE_ARGS ${shaderc_ARGS}
+)
