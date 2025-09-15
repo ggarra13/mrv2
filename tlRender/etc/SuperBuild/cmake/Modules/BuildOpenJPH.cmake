921cd2ca911202dfa1a83c7d74a4af4078af5a52
Gonzalo Garramuño <ggarra13@gmail.com>
2025-09-15 10:10:29 -0300

    Test with patch.

--- tlRender/etc/SuperBuild/cmake/Modules/BuildOpenJPH.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/BuildOpenJPH.cmake
@@ -3,29 +3,24 @@ include(ExternalProject)
 set(OpenJPH_GIT_REPOSITORY "https://github.com/aous72/OpenJPH.git")
 set(OpenJPH_GIT_TAG "0.23.0")
 
-set( OpenJPH_C_COMPILER $ENV{NATIVE_C_COMPILER})
-set( OpenJPH_CXX_COMPILER $ENV{NATIVE_CXX_COMPILER})
-
-
-if (WIN32)
-    # \@bug: Currently, OpenJPH does not build with the Microsoft ARM64
-    #        compiler
-    if ($ENV{ARCH} MATCHES ".*aarch64.*" OR $ENV{ARCH} MATCHES ".*arm64.*")
-	set( OpenJPH_C_COMPILER $ENV{GENERIC_C_COMPILER})
-	set( OpenJPH_CXX_COMPILER $ENV{GENERIC_CXX_COMPILER})
-    endif()
-endif()
-
 set(OpenJPH_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
-    -DCMAKE_C_COMPILER=${OpenJPH_C_COMPILER}
-    -DCMAKE_CXX_COMPILER=${OpenJPH_CXX_COMPILER})
+)
+
+set(OpenJPH_PATH )
+if(WIN32)
+    list(APPEND OpenJPH_PATCH COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/patches/OpenJPH-patch/src/core/common/ojph_arch.h
+        ${CMAKE_CURRENT_BINARY_DIR}/OpenJPH/src/OpenJPH/src/core/common)
+endif()
 
 ExternalProject_Add(
     OpenJPH
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/OpenJPH
     GIT_REPOSITORY ${OpenJPH_GIT_REPOSITORY}
     GIT_TAG ${OpenJPH_GIT_TAG}
+    
+    PATCH_COMMAND ${OpenJPH_PATCH}
     
     LIST_SEPARATOR |
     CMAKE_ARGS ${OpenJPH_ARGS})
