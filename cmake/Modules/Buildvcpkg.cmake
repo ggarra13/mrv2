31a8797d3ec588fd81ba1b9bee88ef8f96d1a3c7
Gonzalo Garramuño <ggarra13@gmail.com>
2025-09-16 08:39:30 -0300

    Added Buildvcpkg.cmake.

---
+++ cmake/Modules/Buildvcpkg.cmake
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# mrv2 
+# Copyright Contributors to the mrv2 Project. All rights reserved.
+
+include( ExternalProject )
+
+set(vcpkg_REPOSITORY "https://github.com/Microsoft/vcpkg.git")
+set(vcpkg_TAG 2025.08.27)
+
+ExternalProject_Add(
+    vcpkg
+    GIT_REPOSITORY ${vcpkg_REPOSITORY}
+    GIT_TAG ${vcpkg_TAG}
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/deps/vcpkg
+    CONFIGURE_COMMAND bootstrap-vcpkg.bat -disableMetrics
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
+    BUILD_IN_SOURCE 1
+)
+
+set( VCPKG ${CMAKE_CURRENT_BINARY_DIR}/deps/vcpkg/vkpkg.exe)
+set( vcpkg_DEP vcpkg )
