5b8e5763565c0aa5cb1a4c589c0cd23fb7c59a28
Gonzalo Garramuño <ggarra13@gmail.com>
2025-09-20 22:11:23 -0300

    Compile openssl but *AFTER* libiconv.

--- tlRender/etc/SuperBuild/cmake/Modules/BuildOpenSSL.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/BuildOpenSSL.cmake
@@ -80,7 +80,7 @@ else()
 		OpenSSL ALL
 		${CMAKE_COMMAND} -E echo "Installing openssl..."
 		COMMAND ${VCPKG} install openssl:${VCPKG_TARGET_TRIPLET}
-		DEPENDS ${vcpkg_DEP})
+		DEPENDS ${vcpkg_DEP} libiconv)
 	else()
 	    ExternalProject_Add(
 		OpenSSL
