03e0c28a5ce5a19a845325215d3bcac8f89895dc
Gonzalo Garramuño <ggarra13@gmail.com>
2025-10-05 20:48:58 -0300

    Bumped version.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -3,9 +3,9 @@
 # Copyright Contributors to the mrv2 Project. All rights reserved.
 
 
-set( mrv2_VERSION_MAJOR 1 )
-set( mrv2_VERSION_MINOR 4 )
-set( mrv2_VERSION_PATCH 3 )
-set( mrv2_VERSION
+set(mrv2_VERSION_MAJOR 1 )
+set(mrv2_VERSION_MINOR 4 )
+set(mrv2_VERSION_PATCH 3 )
+set(mrv2_VERSION
     "${mrv2_VERSION_MAJOR}.${mrv2_VERSION_MINOR}.${mrv2_VERSION_PATCH}"
 )
