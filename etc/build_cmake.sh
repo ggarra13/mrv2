e010781f1b837098f706622e862ab837b7633626
Gonzalo Garramuño <ggarra13@gmail.com>
2025-10-06 13:57:28 -0300

    Fixed build cmake.

--- etc/build_cmake.sh
+++ etc/build_cmake.sh
@@ -8,27 +8,30 @@ if [[ !$RUNME ]]; then
     . ./etc/build_dir.sh
 fi
 
-if [[ ! -d $PWD/$BUILD_DIR/install ]]; then
-    mkdir -p $PWD/$BUILD_DIR/install
+root=$PWD
+
+if [[ ! -d $root/$BUILD_DIR/install ]]; then
+    mkdir -p $root/$BUILD_DIR/install
 fi
 
-if [[ -e $PWD/$BUILD_DIR/install/bin/cmake ]]; then
+if [[ -e $root/$BUILD_DIR/install/bin/cmake ]]; then
     return
 fi
 
-mkdir -p $PWD/$BUILD_DIR/deps
+mkdir -p $root/$BUILD_DIR/deps
 
-cd $PWD/$BUILD_DIR/deps
+cd $root/$BUILD_DIR/deps
 git clone https://gitlab.kitware.com/cmake/cmake.git
 
 #
 # Working checkout, instead of master
 #
+cd cmake
 git checkout 55c6b79da6e16b1683c8b2339ad4a60133e93816
 
 mkdir -p _build
 cd _build
-cmake .. -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$PWD/$BUILD_DIR/install
+cmake .. -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=$root/$BUILD_DIR/install
 ninja
 ninja install
 cd ../..
@@ -36,9 +39,9 @@ cd ../..
 #
 # Clean up cmake repository and build
 #
-cd $PWD/$BUILD_DIR/deps
+cd $root/$BUILD_DIR/deps
 rm -rf cmake
 
 echo "Checking executable is there:"
-ls  $PWD/$BUILD_DIR/install/bin
+ls  $root/$BUILD_DIR/install/bin
 echo "Done with building cmake"
