f5124ccd9e8a795b034e0eca351149258e9f02a3
Gonzalo Garramuño <ggarra13@gmail.com>
2025-11-15 14:31:18 -0300

    New dependencies.

--- tlRender/etc/SuperBuild/cmake/Modules/BuildCURL.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/BuildCURL.cmake
@@ -1,12 +1,13 @@
 include(ExternalProject)
 
 set(CURL_GIT_REPOSITORY "https://github.com/curl/curl.git")
-set(CURL_GIT_TAG "curl-8_4_0")
+set(CURL_GIT_TAG "curl-8_15_0")
 
 set(CURL_ARGS
     -DBUILD_CURL_EXE=OFF
     -DBUILD_TESTING=OFF
     -DCURL_USE_OPENSSL=ON
+    -DCURL_USE_LIBPSL=OFF
     -DCURL_USE_LIBSSH2=ON
     -DCURL_DISABLE_LDAP=ON
     ${TLRENDER_EXTERNAL_ARGS})
