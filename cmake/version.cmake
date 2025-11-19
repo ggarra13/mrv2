b7e8c476f5205ab86e63a0f6b1035a1aad707f74
Gonzalo Garramuño <ggarra13@gmail.com>
2025-11-19 18:30:45 -0300

    Reverted to v1.4.6.

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,11 +1,11 @@
-# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-License-Idkaentifier: BSD-3-Clause
 # mrv2
 # Copyright Contributors to the mrv2 Project. All rights reserved.
 
 
 set(mrv2_VERSION_MAJOR 1 )
 set(mrv2_VERSION_MINOR 4 )
-set(mrv2_VERSION_PATCH 7 )
+set(mrv2_VERSION_PATCH 6 )
 set(mrv2_VERSION
     "${mrv2_VERSION_MAJOR}.${mrv2_VERSION_MINOR}.${mrv2_VERSION_PATCH}"
 )
