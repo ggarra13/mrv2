0a986851bbc73a193d4a65b54f7db9e0ae548ad0
Gonzalo Garramuño <ggarra13@gmail.com>
2025-11-20 14:51:43 -0300

    New FLTK.

--- cmake/Modules/BuildFLTK.cmake
+++ cmake/Modules/BuildFLTK.cmake
@@ -15,7 +15,7 @@ if(TLRENDER_VK)
 	set(FLTK_GIT_REPOSITORY "https://github.com/ggarra13/fltk.git")
     endif()
     
-    set(FLTK_GIT_TAG v1.1.4)
+    set(FLTK_GIT_TAG v1.1.7)
     #set(FLTK_GIT_TAG vk)  # Cutting edge!
     #set(FLTK_GIT_TAG vk_test) # Testing branch
 else()
