1b7b2f7721477d9baa1da166e800a36147c2d349
Gonzalo Garramuño <ggarra13@gmail.com>
2026-02-19 05:51:05 -0300

    Removed ScRgb commented menu.

--- src/lib/mrvUI/mrvMenus.cpp
+++ src/lib/mrvUI/mrvMenus.cpp
@@ -810,20 +810,6 @@ namespace mrv
                 item = (Fl_Menu_Item*)&(menu->menu()[idx]);
                 item->set();
             }
-            
-            // mode = FL_MENU_TOGGLE;
-            // if (numFiles == 0)
-            //     mode |= FL_MENU_INACTIVE;
-
-            // idx = menu->add(_("Render/HDR/Enable ScRGB"),
-            //                 kToggleHDRScRGB.hotkey(),
-            //                 (Fl_Callback*) toggle_hdr_ScRGB_cb, ui,
-            //                 mode);
-            // if (hdrOptions.ScRGB)
-            // {
-            //     item = (Fl_Menu_Item*)&(menu->menu()[idx]);
-            //     item->set();
-            // }
 #endif
             
             mode = FL_MENU_RADIO;
