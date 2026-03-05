e49de2fc29516ea8255032c9901e73b1160e8ab9
Gonzalo Garramuño <ggarra13@gmail.com>
2026-03-04 22:20:01 -0300

    Added monitor capabilities struct.

---
+++ tlRender/lib/tlCore/Monitor.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <tlCore/Vector.h>
+
+namespace tl
+{
+    namespace monitor
+    {
+        struct Capabilities
+        {
+            bool hdr_supported = false;
+            bool hdr_enabled = false;
+            
+            //! Limits.
+            float min_nits = 0.001F;
+            float max_nits = 100.F;
+
+            //! Chromaticities.
+            math::Vector2f red;
+            math::Vector2f green;
+            math::Vector2f blue;
+            math::Vector2f white;
+        };
+    }
+}
