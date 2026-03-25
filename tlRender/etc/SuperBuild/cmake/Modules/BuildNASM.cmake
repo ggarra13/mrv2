bb51a9000b190ddade1625fa2d863ebc6b310c09
Gonzalo Garramuño <ggarra13@gmail.com>
2026-03-25 18:31:22 -0300

    Fixed NASM compilation.

--- tlRender/etc/SuperBuild/cmake/Modules/BuildNASM.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/BuildNASM.cmake
@@ -7,7 +7,7 @@ ExternalProject_Add(
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/NASM
     GIT_REPOSITORY https://github.com/netwide-assembler/nasm
     GIT_TAG ${NASM_GIT_TAG}
-    CONFIGURE_COMMAND autogen.sh && configure
+    CONFIGURE_COMMAND sh autogen.sh && sh configure
     BUILD_COMMAND make
     INSTALL_COMMAND cp nasm ${CMAKE_INSTALL_PREFIX}/bin
     BUILD_IN_SOURCE 1)
