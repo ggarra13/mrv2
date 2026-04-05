6d142624b1089ca8bca96a81885f504789bcf1f7
Gonzalo Garramuño <ggarra13@gmail.com>
2026-04-05 20:35:44 -0300

    Compiles.

--- src/tests/openusd/main.cpp
+++ src/tests/openusd/main.cpp
@@ -1,8 +1,28 @@
 #define USE_GL 1
 
+#include <pxr/pxr.h>
+#include <pxr/base/tf/diagnostic.h>
+#include <pxr/base/tf/token.h>
+#include <pxr/usd/usd/timeCode.h>
+#include <pxr/usd/usd/primRange.h>
+#include <pxr/usd/usdGeom/camera.h>
+#include <pxr/usd/usdGeom/bboxCache.h>
+#include <pxr/usd/usdGeom/metrics.h>
+#include <pxr/usd/usdUtils/pipeline.h>
+#include <pxr/usdImaging/usdAppUtils/api.h>
+#include <pxr/usdImaging/usdAppUtils/camera.h>
+#include <pxr/usdImaging/usdAppUtils/frameRecorder.h>
+#include <pxr/imaging/hd/renderBuffer.h>
+#include <pxr/imaging/hdSt/hioConversions.h>
+#include <pxr/imaging/hdSt/textureUtils.h>
+#include <pxr/imaging/hdx/tokens.h>
+#include <pxr/imaging/hdx/types.h>
+
 #include <tlVk/Mesh.h>
 #include <tlVk/OffscreenBuffer.h>
 #include <tlVk/Shader.h>
+
+#include <tlCore/Path.h>
 
 #if USE_GL
 
@@ -20,10 +40,98 @@
 #include <FL/Fl_Window.H>
 #include <FL/math.h>
 
-#include <iostream>
-#include <limits>
 #include <FL/Fl_Vk_Window.H>
 #include <FL/Fl_Vk_Utils.H>
+
+#include <iostream>
+#include <string>
+
+namespace
+{
+    using namespace pxr;
+    
+    UsdGeomCamera getCamera(
+        const UsdStageRefPtr& stage,
+        const std::string& name = std::string())
+    {
+        UsdGeomCamera out;
+        if (!name.empty())
+        {
+            out = UsdAppUtilsGetCameraAtPath(stage, SdfPath(name));
+        }
+        if (!out)
+        {
+            const TfToken primaryCameraName =
+                UsdUtilsGetPrimaryCameraName();
+            out = UsdAppUtilsGetCameraAtPath(
+                stage, SdfPath(primaryCameraName));
+        }
+        if (!out)
+        {
+            for (const auto& prim : stage->Traverse())
+            {
+                if (prim.IsA<UsdGeomCamera>())
+                {
+                    out = UsdGeomCamera(prim);
+                    break;
+                }
+            }
+        }
+        return out;
+    }
+
+    GfCamera getCameraToFrameStage(
+        const UsdStagePtr& stage, UsdTimeCode timeCode,
+        const TfTokenVector& includedPurposes)
+    {
+        GfCamera gfCamera;
+        UsdGeomBBoxCache bboxCache(timeCode, includedPurposes, true);
+        const GfBBox3d bbox =
+            bboxCache.ComputeWorldBound(stage->GetPseudoRoot());
+        const GfVec3d center = bbox.ComputeCentroid();
+        const GfRange3d range = bbox.ComputeAlignedRange();
+        const GfVec3d dim = range.GetSize();
+        const TfToken upAxis = UsdGeomGetStageUpAxis(stage);
+
+        GfVec2d planeCorner;
+        if (upAxis == UsdGeomTokens->y)
+        {
+            planeCorner = GfVec2d(dim[0], dim[1]) / 2;
+        }
+        else
+        {
+            planeCorner = GfVec2d(dim[0], dim[2]) / 2;
+        }
+        const float planeRadius = sqrt(GfDot(planeCorner, planeCorner));
+
+        const float halfFov =
+            gfCamera.GetFieldOfView(GfCamera::FOVHorizontal) / 2.0;
+        float distance = planeRadius / tan(GfDegreesToRadians(halfFov));
+
+        if (upAxis == UsdGeomTokens->y)
+        {
+            distance += dim[2] / 2;
+        }
+        else
+        {
+            distance += dim[1] / 2;
+        }
+
+        GfMatrix4d xf;
+        if (upAxis == UsdGeomTokens->y)
+        {
+            xf.SetTranslate(center + GfVec3d(0, 0, distance));
+        }
+        else
+        {
+            xf.SetRotate(GfRotation(GfVec3d(1, 0, 0), 90));
+            xf.SetTranslateOnly(center + GfVec3d(0, -distance, 0));
+        }
+        gfCamera.SetTransform(xf);
+        return gfCamera;
+    }
+
+} // namespace
 
 class usd_window : public Fl_Gl_Window
 {
@@ -31,10 +139,22 @@ public:
     usd_window(int X, int Y, int W, int H);
 
     void draw() override;
+
+    void setUSDFile(const std::string&);
+
+    TLRENDER_PRIVATE();
+};
+
+using namespace tl;
+
+struct usd_window::Private
+{
+    file::Path path;
 };
 
 usd_window::usd_window(int X, int Y, int W, int H) :
-    Fl_Gl_Window(X, Y, W, H)
+    Fl_Gl_Window(X, Y, W, H),
+    _p(new Private)
 {
     mode(FL_RGB | FL_ALPHA | FL_DEPTH | FL_STENCIL | FL_OPENGL3);
 }
@@ -51,8 +171,15 @@ void usd_window::draw()
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
+void usd_window::setUSDFile(const std::string& usdFile)
+{
+    TLRENDER_P();
+
+    p.path = file::Path(usdFile);
+}
+
 int main(int argc, char **argv) {
-    if (argc < 2)
+    if (argc != 2)
     {
         std::cerr << argv[0] << " <file.usd>" << std::endl;
         exit(1);
@@ -63,9 +190,10 @@ int main(int argc, char **argv) {
     Fl_Window window(300, 330);
   
     usd_window sw(10, 10, 280, 280);
+    sw.setUSDFile(argv[1]);
 
     window.end();
-    window.show(argc,argv);
+    window.show();
         
     return Fl::run();
 }
