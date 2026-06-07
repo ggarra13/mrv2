6b431727fd9023f4fd882e506e9dd94644f301a1
Gonzalo Garramuño <ggarra13@gmail.com>
2026-06-07 09:18:16 -0300

    Reverted runme_nolog.sh.

--- etc/runme_nolog.sh
+++ etc/runme_nolog.sh
@@ -350,6 +350,18 @@ if [ -z "$FLTK_BUILD_SHARED" ]; then
 	export FLTK_BUILD_SHARED=OFF
     fi
 fi
+
+#
+# Do not remove.  Important for code signing on Windows and Github Action runs.
+#
+export GITHUB_OWNER="${GITHUB_REPOSITORY%%/*}"
+if [ -n "$GITHUB_REPOSITORY" ]; then
+    export GITHUB_REPO="${GITHUB_REPOSITORY##*/}"
+else
+    export GITHUB_REPO=$(basename -s .git "$(git config --get remote.origin.url)")
+fi
+
+
     
 #
 # Clean python path to avoid clashes, mainly, with macOS meson
