c848e1f27ab9e5640ea23f9d2d65c55d4d4d57dd
Gonzalo Garramuño <ggarra13@gmail.com>
2026-06-10 16:05:12 -0300

    Marked version without tablet support.

--- cmake/Modules/BuildFLTK.cmake
+++ cmake/Modules/BuildFLTK.cmake
@@ -4,7 +4,7 @@
 
 include( ExternalProject )
 
-set(FLTK_GIT_TAG v1.9.8)  # was v1.9.5
+set(FLTK_GIT_TAG v1.9.8)  # v1.9.8 has no tablet support
 
 #set(FLTK_GIT_TAG vk)  # Cutting edge!
 #set(FLTK_GIT_TAG vk_merge) # Testing branch
