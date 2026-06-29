6dec189e47624136e7ece455104b06420d980a27
Gonzalo Garramuño <ggarra13@gmail.com>
2026-06-29 19:13:01 -0300

    Added empty NASM_DEP when nasm is not built.

--- tlRender/etc/SuperBuild/cmake/Modules/BuildNASM.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/BuildNASM.cmake
@@ -1,23 +1,26 @@
-include(ExternalProject)
+
+set(NASM_DEP )
 
 if (USE_SYSTEM_LIBS)
     find_program(NASM_EXECUTABLE nasm NAMES nasm.exe nasm)
-    set(NASM_DEP )
 endif()
 
 if (NOT NASM_EXECUTABLE)
+    include(ExternalProject)
+
+    set(NASM_GIT_REPOSITORY "https://github.com/netwide-assembler/nasm")
     set(NASM_GIT_TAG nasm-3.02rc3)
 
     ExternalProject_Add(
 	NASM
 	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/NASM
-	GIT_REPOSITORY https://github.com/netwide-assembler/nasm
+	GIT_REPOSITORY ${NASM_GIT_REPOSITORY}
 	GIT_TAG ${NASM_GIT_TAG}
 	CONFIGURE_COMMAND sh autogen.sh && sh configure
 	BUILD_COMMAND make
 	INSTALL_COMMAND cp nasm ${CMAKE_INSTALL_PREFIX}/bin
 	BUILD_IN_SOURCE 1)
-
+    
     set(NASM_EXECUTABLE ${CMAKE_INSTALL_PREFIX}/bin/nasm)
     set(NASM_DEP NASM)
 endif()
