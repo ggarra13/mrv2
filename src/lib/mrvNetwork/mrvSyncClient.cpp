4a4ec6be50357edc9813a45bd09389eeb5cfb293
Gonzalo Garramuño <ggarra13@gmail.com>
2026-07-07 10:14:23 -0300

    Removed TCP debugging in syncClient.

--- src/lib/mrvNetwork/mrvSyncClient.cpp
+++ src/lib/mrvNetwork/mrvSyncClient.cpp
@@ -39,9 +39,6 @@ namespace mrv
 
         int value;
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
-
         // Sync media
         auto model = ui->app->filesModel();
         auto fileItems = model->observeFiles()->get();
@@ -50,8 +47,6 @@ namespace mrv
         items.reserve(fileItems.size());
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
         for (const auto& fileItem : fileItems)
         {
             const file::Path& path = (*fileItem).path;
@@ -67,50 +62,35 @@ namespace mrv
         pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
         // Sync main UI elements
         syncUI();
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
         // Sync panels
         panel::syncPanels();
 
-
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
         // Set the current file index
         msg["command"] = "Set A Index";
         msg["value"] = model->observeAIndex()->get();
         pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
         // Set the comparison file indexes
         msg["command"] = "Set B Indexes";
         msg["value"] = model->observeBIndexes()->get();
         pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
         // Set the current file index
         msg["command"] = "Set Stereo Index";
         msg["value"] = model->observeStereoIndex()->get();
         pushMessage(msg);
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
 
         // Sync current player with all its information
         if (player)
         {
 
-            std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                      << tcp->isLocked() << std::endl;
             // Send all annotations of current player
             msg["command"] = "Annotations";
             const auto& annotationsPtr = player->getAllAnnotations();
@@ -125,46 +105,34 @@ namespace mrv
             }
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             msg["value"] = annotations;
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Send Environment Map Options
             msg["command"] = "setEnvironmentMapOptions";
             msg["value"] = view->getEnvironmentMapOptions();
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Send Background Options
             msg["command"] = "setBackgroundOptions";
             msg["value"] = view->getBackgroundOptions();
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Send Compare Options
             msg["command"] = "setCompareOptions";
             msg["value"] = model->observeCompareOptions()->get();
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Send Stereo 3D Options
             msg["command"] = "setStereo3DOptions";
             msg["value"] = model->observeStereo3DOptions()->get();
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Set Edit mode
             msg["command"] = "setEditMode";
             EditMode mode = (editMode == EditMode::kFull)
@@ -175,16 +143,12 @@ namespace mrv
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Seek to current time in player
             msg["command"] = "seek";
             msg["value"] = player->currentTime();
             pushMessage(msg);
 
 
-        std::cerr << __FUNCTION__ << " " << __LINE__ << " locked="
-                  << tcp->isLocked() << std::endl;
             // Send playback (as it does not compare if equal, we don't need
             // to send it manually with a message).
             auto playback = player->playback();
