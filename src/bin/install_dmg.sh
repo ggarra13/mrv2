640979cfe565ea176071b349f6ba5d632cfd433b
Gonzalo Garramuño <ggarra13@gmail.com>
2026-09-19 11:50:07 -0300

    Updated install_dmg.sh script to work without installation.command.

--- src/bin/install_dmg.sh
+++ src/bin/install_dmg.sh
@@ -10,7 +10,7 @@ DMG_NAME="${DMG_BASENAME%.dmg}"
 DMG_PREFIX="${DMG_NAME%%-*}"
 
 # Remove the quarantine attribute from DMG
-xattr -d com.apple.quarantine "$DMG_FILE"
+# xattr -d com.apple.quarantine "$DMG_FILE"
 
 # Open the .dmg file
 echo "Open $DMG_FILE"
@@ -38,25 +38,29 @@ if [[ -e /Volumes/${DMG_NAME}/hdr.app ]]; then
     cp -rf /Volumes/${DMG_NAME}/hdr.app /Applications/
 fi
 
-echo "Running installation.command script to finish installation..."
-open /Volumes/${DMG_NAME}/installation.command
+if [[ -e /Volumes/${DMG_NAME}/installation.command ]]; then
+    
+    echo "Running installation.command script to finish installation..."
 
-# 1. Wait up to 15 seconds for the new Terminal window to actually start the
-#    script
-echo "Waiting for Terminal to launch..."
-for i in {1..15}; do
-    if pgrep -f "/Volumes/${DMG_NAME}/installation.command" > /dev/null; then
-        # The process has started, break out of this initial waiting loop
-        break
-    fi
-    sleep 1
-done
+    open /Volumes/${DMG_NAME}/installation.command
 
-# 2. Now wait for the script to finish and the process to disappear
-echo "Waiting for installation to complete (please enter your password in the new window)..."
-while pgrep -f "/Volumes/${DMG_NAME}/installation.command" > /dev/null; do
-    sleep 1
-done
+    # 1. Wait up to 15 seconds for the new Terminal window to actually start the
+    #    script
+    echo "Waiting for Terminal to launch..."
+    for i in {1..15}; do
+	if pgrep -f "/Volumes/${DMG_NAME}/installation.command" > /dev/null; then
+            # The process has started, break out of this initial waiting loop
+            break
+	fi
+	sleep 1
+    done
+
+    # 2. Now wait for the script to finish and the process to disappear
+    echo "Waiting for installation to complete (please enter your password in the new window)..."
+    while pgrep -f "/Volumes/${DMG_NAME}/installation.command" > /dev/null; do
+	sleep 1
+    done
+fi
 
 # When you're done, unmount the .dmg (optional)
 hdiutil detach "/Volumes/${DMG_NAME}"
