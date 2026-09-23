f505da48f01e7c46a807af88493eb1dbebd76587
Gonzalo Garramuño <ggarra13@gmail.com>
2026-09-23 11:15:53 -0300

    Updated comment.

--- tlRender/etc/SuperBuild/cmake/Modules/BuildVPX.cmake
+++ tlRender/etc/SuperBuild/cmake/Modules/BuildVPX.cmake
@@ -11,7 +11,9 @@ if (NOT VPX_FOUND AND NOT FFmpeg_FOUND)
 
     # set(LIBVPX_TAG main) # live on the cutting-edge!
 
-    set(VPX_TAG v1.17.0)  # was v1.15.2
+    set(VPX_TAG v1.17.0)  # was v1.15.2,
+                          # v1.16.0 which compiled on Win64 is now broken on
+                          # GitHub.
 
     include(ProcessorCount)
     ProcessorCount(NPROCS)
